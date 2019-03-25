0359fad08b9bd93288304531b7ad6675aa02b41e
Michael Lynch <mtlynch@users.noreply.github.com>
2019-03-25 18:08:22 -0400

    Changing Vue implementation to npm package (#1)

---
+++ client/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Journal v-for="item in journalEntries" v-bind:entry="item" v-bind:key="item.id"/>
+  </div>
+</template>
+
+<script>
+import Journal from "./components/Journal.vue";
+
+export default {
+  name: "app",
+  components: {
+    Journal
+  },
+  data() {
+    return {
+      journalEntries: [
+        { id: 0, date: "2019-03-25", markdown: "Ate some crackers" },
+        { id: 1, date: "2019-03-24", markdown: "Took a nap" },
+        { id: 2, date: "2019-03-23", markdown: "Watched a movie" }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Journal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="journal">
+    <div class="journalEntry">
+      <div class="date">{{ entry.date }}</div>
+      <div class="journalBody">{{ entry.markdown }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Journal",
+  props: {
+    entry: Object
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
