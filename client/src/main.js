f2a2e8a0b2ca06083b70bd0d0e622eb41677599d
Michael Lynch <git@mtlynch.io>
2019-03-25 18:31:35 -0400

    Adding Axios and moving data to backend

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   render: h => h(App),
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
@@ -8,7 +9,12 @@ import (
 
 type Page struct {
 	Title string
-	Body  string
+}
+
+type JournalEntry struct {
+	Id       int    `json:"name"`
+	Date     string `json:"date"`
+	Markdown string `json:"markdown"`
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request, title string) {
@@ -36,10 +42,23 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
+func entriesHandler(w http.ResponseWriter, r *http.Request) {
+	entries := []JournalEntry{
+		JournalEntry{Id: 0, Date: "2019-03-25", Markdown: "Ate some crackers"},
+		JournalEntry{Id: 1, Date: "2019-03-24", Markdown: "Took a nap"},
+		JournalEntry{Id: 2, Date: "2019-03-23", Markdown: "Watched a movie!"},
+	}
+
+	if err := json.NewEncoder(w).Encode(entries); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	fs := http.FileServer(http.Dir("client/dist"))
 	http.Handle("/css/", fs)
 	http.Handle("/js/", fs)
+	http.HandleFunc("/entries", entriesHandler)
 	http.HandleFunc("/", makeHandler(indexHandler))
 	log.Fatal(http.ListenAndServe(":3001", nil))
 }
