7b22d2a8ca6d12dedb89affeba6de175ad03b8de
Michael Lynch <git@mtlynch.io>
2019-04-05 15:42:02 -0400

    Switching to Vue routes

---
+++ client/src/components/Submit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="submit">
+    <p>Submit your thing.</p>
+    <form>
+      <textarea name="snippet" rows="5"></textarea>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Submit"
+};
+</script>
+
+<style scoped>
+.submit {
+  font-size: 11pt;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
+import Submit from "./components/Submit.vue";
+import ViewEntry from "./components/ViewEntry.vue";
+import MissingPage from "./components/404.vue";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,6 +15,20 @@ Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
 
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/:username/:date', component: ViewEntry },
+  { path: '/submit', component: Submit },
+  { path: '*', component: MissingPage },
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
