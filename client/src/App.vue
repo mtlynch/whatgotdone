a7701f8b11a87e3e15fca4c05006262b2f47d0ef
Michael Lynch <git@mtlynch.io>
2019-04-05 11:32:50 -0400

    Adding bootstrap

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <p>
       Journal entry for
       <b>{{ username }}</b> on
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
