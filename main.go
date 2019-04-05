e7f79645864f43479873044148403a1c6419b13d
Michael Lynch <git@mtlynch.io>
2019-04-05 18:29:07 -0400

    Switching long entries to text files, adding new snippet

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -46,67 +47,7 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 	}
 }
 
-var realEntry = `
-# [IsItKeto](https://isitketo.org)
-
-* Migrated to Circle
-* Migrated from Selenium to Cypress
-* Fixed generation of structured data
-* Migrated admin interface to Circle
-* Wrote a script to find missing crosslinks
-* Wrote a script to download the datastore locally
-* Commissioned cartoonist to create new keto meter graphics
-* Tried reaching out to KetoSizeMe about visualizations but no dice
-* Checked in script for making Tweet spreadsheet
-* Fixed a bunch of errors in the NYT data
-* At 439 Twitter followers (forgot to check last week)
-* Still at 173 foods
-
-# Conferences
-
-* Mostly finished Good Developers Bad Tests presentation
-
-# [Zestful](https://zestfuldata.com)
-
-* Researched Sizzle app as potential customer
-* Reached out to author of forked NYT repo
-* Reached out to UPenn student and set up private Zestful instance
-  * Added README instructions for creating a new private instance
-* Attempted to retrain, but accuracy dropped
-  * Still, 200+ new labeled examples
-* Stripped out “homemade or store-bought” from input
-
-# What Got Done App
-
-* Got a basic version working
-  * Successfully runs Golang on the backend and Vue.js on the frontend and they can communicate
-  * No database or deployments yet
-
-# [mtlynch.io](https://mtlynch.io)
-
-* Removed G+ social sharing button
-
-# [Dusty VCR](https://dustyvcr.com)
-
-* Moved RSS feed to Feedburner
-
-# Misc
-
-* Finished Indie Jewel Thieves podcast
-* Talked to reporter
-* Finished working with accountant on taxes
-* Attended first beekeeping workshop
-* Submitted pull requests to Cypress
-  * [Load test spec at runtime rather than Docker image build time](https://github.com/cypress-io/cypress-example-docker-compose/pull/5)
-  * [Adding Cypress artifacts folders to .gitignore](https://github.com/cypress-io/cypress-example-docker-compose/pull/4)
-`
-
 var entries = []JournalEntry{
-	JournalEntry{
-		Date:         "2019-03-29",
-		LastModified: "2019-03-29T23:34:02.111Z",
-		Markdown:     realEntry,
-	},
 	JournalEntry{
 		Date:         "2019-03-22",
 		LastModified: "2019-03-22T08:15:22.382Z",
@@ -126,6 +67,7 @@ var entries = []JournalEntry{
 
 func entriesHandler(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
+
 	if err := json.NewEncoder(w).Encode(entries); err != nil {
 		panic(err)
 	}
@@ -175,7 +117,31 @@ func enableCsp(w *http.ResponseWriter) {
 	(*w).Header().Set("Content-Security-Policy", "default-src 'self'")
 }
 
+func addTextEntries() {
+	a, err := ioutil.ReadFile("2019-03-29.md")
+	if err != nil {
+		panic(err)
+	}
+	b, err := ioutil.ReadFile("2019-04-05.md")
+	if err != nil {
+		panic(err)
+	}
+	entries = append(entries,
+		JournalEntry{
+			Date:         "2019-04-05",
+			LastModified: "2019-04-05T21:36:05.333Z",
+			Markdown:     string(b),
+		})
+	entries = append(entries,
+		JournalEntry{
+			Date:         "2019-03-29",
+			LastModified: "2019-03-29T23:34:02.111Z",
+			Markdown:     string(a),
+		})
+}
+
 func main() {
+	addTextEntries()
 	fs := http.FileServer(http.Dir("client/dist"))
 	http.Handle("/css/", fs)
 	http.Handle("/js/", fs)
