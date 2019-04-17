7f73245dcc40d00db3624a5f35e1d64c3e9c2006
Michael Lynch <git@mtlynch.io>
2019-04-17 17:44:19 -0400

    Fixing tests

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"reflect"
 	"testing"
 
 	"github.com/gorilla/mux"
@@ -10,14 +13,11 @@ import (
 )
 
 type mockDatastore struct {
+	journalEntries []types.JournalEntry
 }
 
 func (ds mockDatastore) All() ([]types.JournalEntry, error) {
-	return []types.JournalEntry{
-		types.JournalEntry{Date: "2019-03-22", LastModified: "2019-03-24", Markdown: "Ate some crackers"},
-		types.JournalEntry{Date: "2019-03-15", LastModified: "2019-03-15", Markdown: "Took a nap"},
-		types.JournalEntry{Date: "2019-03-08", LastModified: "2019-03-09", Markdown: "Watched the movie *The Royal Tenenbaums*."},
-	}, nil
+	return ds.journalEntries, nil
 }
 
 func (ds mockDatastore) InsertJournalEntry(types.JournalEntry) error {
@@ -28,8 +28,23 @@ func (ds mockDatastore) Close() error {
 	return nil
 }
 
+func init() {
+	// The handler uses relative paths to find the template file. Switch to the
+	// app's root directory so that the relative paths work.
+	if err := os.Chdir("../"); err != nil {
+		panic(err)
+	}
+}
+
 func TestEntriesHandler(t *testing.T) {
-	ds := mockDatastore{}
+	entries := []types.JournalEntry{
+		types.JournalEntry{Date: "2019-03-22", LastModified: "2019-03-24", Markdown: "Ate some crackers"},
+		types.JournalEntry{Date: "2019-03-15", LastModified: "2019-03-15", Markdown: "Took a nap"},
+		types.JournalEntry{Date: "2019-03-08", LastModified: "2019-03-09", Markdown: "Watched the movie *The Royal Tenenbaums*."},
+	}
+	ds := mockDatastore{
+		journalEntries: entries,
+	}
 	router := mux.NewRouter()
 	s := defaultServer{
 		datastore: ds,
@@ -42,12 +57,20 @@ func TestEntriesHandler(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	rr := httptest.NewRecorder()
+	w := httptest.NewRecorder()
+	s.router.ServeHTTP(w, req)
 
-	s.router.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
+	if status := w.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
+	}
+	var response []types.JournalEntry
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Errorf("Response is not valid JSON: %v", w.Body.String())
+	}
+
+	if !reflect.DeepEqual(response, entries) {
+		t.Fatalf("Unexpected response: got %v want %v", response, entries)
 	}
 }
