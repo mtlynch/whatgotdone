5b06ac8bb09a3825d202ed7d513592da56c26068
Michael Lynch <mtlynch@users.noreply.github.com>
2019-05-07 13:35:36 -0500

    Adding homepage (#66)

---
+++ web/frontend/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <p>What Got Done is a task journaling app.</p>
+    <p>Write a weekly summary of everything you did and share it publicly.</p>
+    <p>
+      Check out an example entry like
+      <a href="/michael/2019-05-03">this one</a>.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
