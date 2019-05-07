6164d71577412a2f7a8b876641eb0196dbd039fc
Michael Lynch <git@mtlynch.io>
2019-05-07 18:15:22 -0400

    Adding a separate dev config for Firebase + UserKit

--- web/frontend/src/components/Login.vue
+++ web/frontend/src/components/Login.vue
@@ -10,7 +10,10 @@ export default {
   mounted() {
     let userKitScript = document.createElement("script");
     userKitScript.setAttribute("src", "https://widget.userkit.io/widget.js");
-    userKitScript.setAttribute("data-app-id", "app_zXjb2TF8");
+    userKitScript.setAttribute(
+      "data-app-id",
+      process.env.VUE_APP_USERKIT_APP_ID
+    );
     userKitScript.setAttribute("data-show-on-load", "login");
     userKitScript.setAttribute("data-login-dismiss", "false");
     document.head.appendChild(userKitScript);
