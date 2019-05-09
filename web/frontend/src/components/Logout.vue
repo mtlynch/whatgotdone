401521ad7d0b50e430fc3098afef6affd1935c29
Michael Lynch <mtlynch@users.noreply.github.com>
2019-05-09 16:11:43 -0500

    Adding a navigation bar (#77)

---
+++ web/frontend/src/components/Logout.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>Logout functionality coming soon!</p>
+  </div>
+</template>
---
+++ web/frontend/src/components/ProUpgrade.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>You should upgrade to Pro!</p>
+    <p>Soon, we'll tell you why.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProUpgrade"
+};
+</script>
