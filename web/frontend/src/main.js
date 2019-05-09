e307133ce538ae6ebe6cb14d325aa3284446fb60
Michael Lynch <mtlynch@users.noreply.github.com>
2019-05-08 22:59:30 -0500

    Making Google Analytics actually work (#71)

--- web/frontend/src/main.js
+++ web/frontend/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import axios from 'axios'
+import VueAnalytics from 'vue-analytics'
 import VueAxios from 'vue-axios'
 import VueMoment from 'vue-moment'
 import VueRouter from 'vue-router'
@@ -15,6 +16,11 @@ import 'bootswatch/dist/superhero/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+if (process.env.VUE_APP_GOOGLE_ANALYTICS_ID) {
+  Vue.use(VueAnalytics, {
+    id: process.env.VUE_APP_GOOGLE_ANALYTICS_ID,
+  })
+}
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
