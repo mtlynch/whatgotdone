e85173c61ceae9bdfb9273cdd2ec43c9dfe72771
Michael Lynch <git@mtlynch.io>
2019-05-10 18:24:27 -0400

    Adding pro features

--- web/frontend/src/components/ProUpgrade.vue
+++ web/frontend/src/components/ProUpgrade.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
+  <div class="pro">
     <p>You should upgrade to Pro!</p>
     <p>Soon, we'll tell you why.</p>
+
+    <p>Pro accounts will probably have snazzy privileges like:</p>
+
+    <ul>
+      <li>Private entries</li>
+      <li>Automatically send weekly updates to teammates</li>
+    </ul>
   </div>
 </template>
 
@@ -10,3 +17,9 @@ export default {
   name: "ProUpgrade"
 };
 </script>
+
+<style scoped>
+.pro {
+  text-align: left;
+}
+</style>
