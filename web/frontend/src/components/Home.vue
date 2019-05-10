52d4984588222c1471e541c2b21d92a30589160b
Michael Lynch <git@mtlynch.io>
2019-05-10 18:20:18 -0400

    Adding more explanation to the homepage

--- web/frontend/src/components/Home.vue
+++ web/frontend/src/components/Home.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="home">
-    <p>What Got Done is a task journaling app.</p>
-    <p>Write a weekly summary of everything you did and share it publicly.</p>
+    <h1>What did you get done this week?</h1>
+    <p>What Got Done is a weekly task journaling app.</p>
+    <p>End each week by reflecting on what you accomplished. It's a great way to reflect on the week and share your progress with others.</p>
     <p>
       Check out some
       <router-link to="/recent">recent entries</router-link>.
+    </p>
+    <h1>Interested?</h1>
+    <p>What Got Done is still a work in progress, and new user registration is not yet open.</p>
+    <p>
+      If you're interested in beta testing the site, please send an email to
+      <a
+        href="mailto:michael@mtlynch.io"
+      >michael@mtlynch.io</a>.
     </p>
   </div>
 </template>
@@ -14,3 +23,17 @@ export default {
   name: "Home"
 };
 </script>
+
+<style scoped>
+.home {
+  text-align: left;
+}
+
+h1 {
+  margin-top: 50px;
+}
+
+p {
+  margin-top: 20px;
+}
+</style>
