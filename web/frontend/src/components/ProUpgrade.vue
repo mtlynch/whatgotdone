873947fdd94ddd7bd324fdc661a8e5adc79617dd
Michael Lynch <git@mtlynch.io>
2019-05-21 18:02:37 -0400

    Adding signup form to What Got Done Pro

--- web/frontend/src/components/ProUpgrade.vue
+++ web/frontend/src/components/ProUpgrade.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="signup">
-    <h1>Find out when What Got Done Pro is available</h1>
-    <p>We're still working on What Got Done Pro. Soon this will be a signup link so you can be notified when it's available.</p>
+  <div class="signup container">
+    <h1>Find out when we launch Pro</h1>
+    <p>We're still working on What Got Done Pro.</p>
+    <p>Enter your email below, and we'll contact you as soon as it's ready.</p>
+    <iframe
+      width="540"
+      height="305"
+      src="https://sibforms.com/serve/MUIEAMN3b7iyDb1Kdu3K4K1BgANAtm0qXVjt7FfMfUVnIC0_4QKbxV3ipaVgSBwA_o4pVrLGBofqCFlsLn-TxuIiorflVuqFXOig9NpjtAUs0nulPn_moczZLgVcdOcPjR9_iPFh6A67aMeVsn1Fj45BkRR5jXzkdEXJyWTyA_Yst6g30GXCDvUv5TM-jbgTCqjI9b7tdsK77K-e"
+      frameborder="0"
+      scrolling="auto"
+      allowfullscreen
+      style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"
+    ></iframe>
   </div>
 </template>
 
@@ -10,3 +20,13 @@ export default {
   name: "ProPitch"
 };
 </script>
+
+<style scoped>
+.signup {
+  text-align: left;
+}
+
+iframe {
+  margin-top: 50px;
+}
+</style>
