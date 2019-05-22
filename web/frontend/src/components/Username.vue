86a8f30dabde83c22cfd02ebdcb468060448d9d6
Michael Lynch <git@mtlynch.io>
2019-05-22 12:00:23 -0400

    Tweaking UI on recent page
    Fixes #101

---
+++ web/frontend/src/components/Username.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="username">
+    <router-link :to="'/' + username">{{ username }}</router-link>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Username",
+  props: {
+    username: String
+  }
+};
+</script>
+
+<style scoped>
+.username a {
+  font-weight: bold;
+  color: rgb(255, 208, 56) !important;
+  text-decoration: none;
+}
+</style>
