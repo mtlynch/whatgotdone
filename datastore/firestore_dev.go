1752c38da104ac1f798e79f98c363259ade908be
Michael Lynch <mtlynch@users.noreply.github.com>
2019-05-24 09:31:10 -0500

    Splitting firestore into client and prod files (#138)
    * Splitting firestore into client and prod files
    
    * Fixing build directive on firestore_dev

---
+++ datastore/firestore_dev.go
@@ -0,0 +1,16 @@
+// +build dev
+
+package datastore
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/option"
+)
+
+func newFirestoreClient(ctx context.Context) (*firestore.Client, error) {
+	const firestoreProjectID = "whatgotdone-dev"
+	const devServiceAccount = "service-account-creds.json"
+	return firestore.NewClient(ctx, firestoreProjectID, option.WithCredentialsFile(devServiceAccount))
+}
---
+++ datastore/firestore_prod.go
@@ -0,0 +1,14 @@
+// +build !dev
+
+package datastore
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+)
+
+func newFirestoreClient(ctx context.Context) (*firestore.Client, error) {
+	const firestoreProjectID = "whatgotdone"
+	return firestore.NewClient(ctx, firestoreProjectID)
+}
