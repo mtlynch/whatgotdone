b81acb2a726a78005e0b10851d6c209abe78cfd3
Michael Lynch <mtlynch@users.noreply.github.com>
2019-05-25 20:00:08 -0500

    Adding more e2e tests (#145)

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -4,3 +4,21 @@ it('loads the homepage', () => {
   cy.get('h1')
     .should('contain', 'What did you get done this week?')
 })
+
+it('views recent posts', () => {
+  cy.visit('/recent')
+
+  cy.get('div.journal').should('contain', 'staging.jimmy\'s update')
+})
+
+it('logs in', () => {
+  cy.visit('/login')
+
+  cy.get('#userkit_username')
+    .type('staging.jimmy')
+  cy.get('#userkit_password')
+    .type('just4st@ginG!')
+  cy.get('form').submit()
+
+  cy.url().should('include', '/submit')
+})
---
+++ handlers/csp_staging.go
@@ -0,0 +1,37 @@
+// +build staging
+
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func enableCsp(w *http.ResponseWriter) {
+	defaultSrc := strings.Join([]string{
+		"'self'",
+		// URLs for /login route
+		"https://widget.userkit.io",
+		"https://api.userkit.io",
+		"https://www.google.com/recaptcha/api.js",
+		"https://www.gstatic.com/recaptcha/api2/",
+		"https://fonts.googleapis.com",
+		"https://fonts.gstatic.com",
+		"https://www.google-analytics.com",
+		"https://www.googletagmanager.com",
+	}, " ")
+	imgSrc := strings.Join([]string{
+		"'self'",
+		// For bootstrap navbar images
+		"data:",
+		// For Google Analytics
+		"https://www.google-analytics.com",
+	}, " ")
+	frameSrc := strings.Join([]string{
+		"'self'",
+		// For sendinblue mailing list signup
+		"https://sibforms.com",
+	}, " ")
+	(*w).Header().Set("Content-Security-Policy", fmt.Sprintf("default-src %s; img-src %s; frame-src %s unsafe-eval", defaultSrc, imgSrc, frameSrc))
+}
