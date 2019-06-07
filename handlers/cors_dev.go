3895ce3946d30bb7a7faf7935ec87f1f56dc652a
Michael Lynch <git@mtlynch.io>
2019-06-07 17:16:45 -0400

    Fixing dev CORS config

--- handlers/cors_dev.go
+++ handlers/cors_dev.go
@@ -6,9 +6,9 @@ import "net/http"
 
 func (s defaultServer) enableCors(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		(*w).Header().Set("Access-Control-Allow-Origin", "http://10.0.75.1:8081")
-		(*w).Header().Set("Access-Control-Allow-Credentials", "true")
-		(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Origin", "http://10.0.75.1:8081")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		h(w, r)
 	}
 }
