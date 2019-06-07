09d4b318d37aa1164ab71856693fc64da9cd84b0
Michael Lynch <git@mtlynch.io>
2019-06-07 16:58:51 -0400

    Organizing middleware in a better way

--- handlers/csp_prod.go
+++ handlers/csp_prod.go
@@ -8,30 +8,33 @@ import (
 	"strings"
 )
 
-func enableCsp(w *http.ResponseWriter) {
-	defaultSrc := strings.Join([]string{
-		"'self'",
-		// URLs for /login route
-		"https://widget.userkit.io",
-		"https://api.userkit.io",
-		"https://www.google.com/recaptcha/api.js",
-		"https://www.gstatic.com/recaptcha/api2/",
-		"https://fonts.googleapis.com",
-		"https://fonts.gstatic.com",
-		"https://www.google-analytics.com",
-		"https://www.googletagmanager.com",
-	}, " ")
-	imgSrc := strings.Join([]string{
-		"'self'",
-		// For bootstrap navbar images
-		"data:",
-		// For Google Analytics
-		"https://www.google-analytics.com",
-	}, " ")
-	frameSrc := strings.Join([]string{
-		"'self'",
-		// For sendinblue mailing list signup
-		"https://sibforms.com",
-	}, " ")
-	(*w).Header().Set("Content-Security-Policy", fmt.Sprintf("default-src %s; img-src %s; frame-src %s", defaultSrc, imgSrc, frameSrc))
+func (s defaultServer) enableCsp(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defaultSrc := strings.Join([]string{
+			"'self'",
+			// URLs for /login route
+			"https://widget.userkit.io",
+			"https://api.userkit.io",
+			"https://www.google.com/recaptcha/api.js",
+			"https://www.gstatic.com/recaptcha/api2/",
+			"https://fonts.googleapis.com",
+			"https://fonts.gstatic.com",
+			"https://www.google-analytics.com",
+			"https://www.googletagmanager.com",
+		}, " ")
+		imgSrc := strings.Join([]string{
+			"'self'",
+			// For bootstrap navbar images
+			"data:",
+			// For Google Analytics
+			"https://www.google-analytics.com",
+		}, " ")
+		frameSrc := strings.Join([]string{
+			"'self'",
+			// For sendinblue mailing list signup
+			"https://sibforms.com",
+		}, " ")
+		w.Header().Set("Content-Security-Policy", fmt.Sprintf("default-src %s; img-src %s; frame-src %s", defaultSrc, imgSrc, frameSrc))
+		h(w, r)
+	}
 }
