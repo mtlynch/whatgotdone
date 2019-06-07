9689479159876d8b2662ac4e025d00cad13f77af
Michael Lynch <git@mtlynch.io>
2019-06-07 16:36:20 -0400

    Changing dev hostname from localhost to 10.0.75.1
    I can't figure out how to get browser to stop bypassing the HTTP proxy for localhost

--- handlers/cors_dev.go
+++ handlers/cors_dev.go
@@ -5,7 +5,7 @@ package handlers
 import "net/http"
 
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://10.0.75.1:8081")
 	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
