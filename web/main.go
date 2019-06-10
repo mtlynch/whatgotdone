37bfab2fa018fc7c5e1197098e232e8026b37424
Michael Lynch <mtlynch@users.noreply.github.com>
2019-06-10 14:46:57 -0500

    Adding request logging to the backend (#177)

--- web/main.go
+++ web/main.go
@@ -6,17 +6,21 @@ import (
 	"net/http"
 	"os"
 
+	muxHandlers "github.com/gorilla/handlers"
+
 	"github.com/mtlynch/whatgotdone/handlers"
 )
 
 func main() {
+	log.Print("Starting whatgotdone server")
 	s := handlers.New()
-	http.Handle("/", s.Router())
+	http.Handle("/", muxHandlers.LoggingHandler(os.Stdout, s.Router()))
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 	log.Printf("Listening on %s", port)
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
