2bcc326431e98a065a690aceae85049c4c6b9580
Michael Lynch <git@mtlynch.io>
2019-07-10 11:07:23 -0400

    Splitting submit handler to its own file

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -258,69 +258,6 @@ func (s defaultServer) userMeHandler() http.HandlerFunc {
 	}
 }
 
-func (s *defaultServer) submitHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "OPTIONS" {
-			return
-		}
-
-		username, err := s.loggedInUser(r)
-		if err != nil {
-			http.Error(w, "You must log in to submit a journal entry", http.StatusForbidden)
-			return
-		}
-
-		type submitRequest struct {
-			Date         string `json:"date"`
-			EntryContent string `json:"entryContent"`
-		}
-
-		type submitResponse struct {
-			Ok   bool   `json:"ok"`
-			Path string `json:"path"`
-		}
-
-		var t submitRequest
-		decoder := json.NewDecoder(r.Body)
-		err = decoder.Decode(&t)
-		if err != nil {
-			log.Printf("Failed to decode request: %s", err)
-			http.Error(w, "Failed to decode request", http.StatusBadRequest)
-		}
-		if !validateEntryDate(t.Date) {
-			log.Printf("Invalid date: %s", t.Date)
-			http.Error(w, "Invalid date", http.StatusBadRequest)
-			return
-		}
-
-		j := types.JournalEntry{
-			Date:         t.Date,
-			LastModified: time.Now().Format(time.RFC3339),
-			Markdown:     t.EntryContent,
-		}
-
-		err = s.datastore.InsertDraft(username, j)
-		if err != nil {
-			log.Printf("Failed to update journal draft entry: %s", err)
-			http.Error(w, "Failed to insert entry", http.StatusInternalServerError)
-			return
-		}
-		err = s.datastore.Insert(username, j)
-		if err != nil {
-			log.Printf("Failed to insert journal entry: %s", err)
-			http.Error(w, "Failed to insert entry", http.StatusInternalServerError)
-			return
-		}
-		resp := submitResponse{
-			Ok:   true,
-			Path: fmt.Sprintf("/%s/%s", username, t.Date),
-		}
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			panic(err)
-		}
-	}
-}
-
 func validateEntryDate(date string) bool {
 	t, err := time.Parse("2006-01-02", date)
 	if err != nil {
---
+++ handlers/submit.go
@@ -0,0 +1,74 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/mtlynch/whatgotdone/types"
+)
+
+func (s *defaultServer) submitHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		username, err := s.loggedInUser(r)
+		if err != nil {
+			http.Error(w, "You must log in to submit a journal entry", http.StatusForbidden)
+			return
+		}
+
+		type submitRequest struct {
+			Date         string `json:"date"`
+			EntryContent string `json:"entryContent"`
+		}
+
+		var t submitRequest
+		decoder := json.NewDecoder(r.Body)
+		err = decoder.Decode(&t)
+		if err != nil {
+			log.Printf("Failed to decode request: %s", err)
+			http.Error(w, "Failed to decode request", http.StatusBadRequest)
+		}
+		if !validateEntryDate(t.Date) {
+			log.Printf("Invalid date: %s", t.Date)
+			http.Error(w, "Invalid date", http.StatusBadRequest)
+			return
+		}
+
+		j := types.JournalEntry{
+			Date:         t.Date,
+			LastModified: time.Now().Format(time.RFC3339),
+			Markdown:     t.EntryContent,
+		}
+
+		err = s.datastore.InsertDraft(username, j)
+		if err != nil {
+			log.Printf("Failed to update journal draft entry: %s", err)
+			http.Error(w, "Failed to insert entry", http.StatusInternalServerError)
+			return
+		}
+		err = s.datastore.Insert(username, j)
+		if err != nil {
+			log.Printf("Failed to insert journal entry: %s", err)
+			http.Error(w, "Failed to insert entry", http.StatusInternalServerError)
+			return
+		}
+
+		type submitResponse struct {
+			Ok   bool   `json:"ok"`
+			Path string `json:"path"`
+		}
+		resp := submitResponse{
+			Ok:   true,
+			Path: fmt.Sprintf("/%s/%s", username, t.Date),
+		}
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			panic(err)
+		}
+	}
+}
