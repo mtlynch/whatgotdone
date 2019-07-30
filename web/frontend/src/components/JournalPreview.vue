25f516350679d8e1f76e107aad9eb3d6ac290c3c
Michael Lynch <git@mtlynch.io>
2019-07-30 11:07:52 -0400

    Adding markdown previews to submit page

---
+++ web/frontend/src/components/JournalPreview.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="journal-preview">
+    <p class="preview-bar">preview</p>
+    <div class="content">
+      <vue-markdown
+        :linkify="false"
+        :html="false"
+        :anchorAttributes="{rel: 'nofollow' }"
+        :source="markdown"
+      ></vue-markdown>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueMarkdown from "vue-markdown";
+
+Vue.use(VueMarkdown);
+
+export default {
+  name: "JournalPreview",
+  props: {
+    markdown: String
+  },
+  components: {
+    VueMarkdown
+  }
+};
+</script>
+
+<style scoped>
+.journal-preview {
+  margin-top: 40px;
+  background-color: #4e5d6c;
+  border: 1px solid rgb(90, 90, 90);
+  text-align: left;
+}
+
+.preview-bar {
+  background-color: #3f464d;
+  font-size: 9pt;
+  padding: 4px 2px 4px 14px;
+}
+
+.content {
+  padding: 4px 14px;
+}
+</style>
