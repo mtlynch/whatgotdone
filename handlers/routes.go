cd6c85cb95b688b5ee032e69056bdcb67922d106
Michael Lynch <git@mtlynch.io>
2019-08-13 19:36:20 -0400

    Splitting up draft handlers into per-method-type handlers

--- handlers/routes.go
+++ handlers/routes.go
@@ -12,7 +12,9 @@ func (s *defaultServer) routes() {
 	s.router.PathPrefix("/app.js").Handler(fs)
 
 	s.router.HandleFunc("/api/entries/{username}", s.enableCors(s.entriesHandler()))
-	s.router.HandleFunc("/api/draft/{date}", s.enableCors(s.draftHandler()))
+	s.router.HandleFunc("/api/draft/{date}", s.enableCors(s.draftOptions())).Methods(http.MethodOptions)
+	s.router.HandleFunc("/api/draft/{date}", s.enableCors(s.draftGet())).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/draft/{date}", s.enableCors(s.draftPost())).Methods(http.MethodPost)
 	s.router.HandleFunc("/api/reactions/entry/{username}/{date}", s.enableCors(s.reactionsHandler()))
 	s.router.HandleFunc("/api/recentEntries", s.enableCors(s.recentEntriesHandler()))
 	s.router.HandleFunc("/api/user/me", s.enableCors(s.userMeHandler()))
