7b1087a8fdae595aee4c9a3cc6f57e5eb0a59a3e
Michael Lynch <mtlynch@users.noreply.github.com>
2019-08-14 10:29:08 -0500

    Refactoring /api/entries to a separate file (#232)
    And making it explicitly for HTTP GET

---
+++ handlers/entries.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func (s *defaultServer) entriesGet() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username, err := usernameFromRequestPath(r)
+		if err != nil {
+			log.Printf("Failed to retrieve username from request path: %s", err)
+			http.Error(w, "Invalid username", http.StatusBadRequest)
+			return
+		}
+
+		entries, err := s.datastore.All(username)
+		if err != nil {
+			log.Printf("Failed to retrieve entries: %s", err)
+			http.Error(w, fmt.Sprintf("Failed to retrieve entries for %s", username), http.StatusInternalServerError)
+			return
+		}
+
+		if err := json.NewEncoder(w).Encode(entries); err != nil {
+			panic(err)
+		}
+	}
+}
