f968e968584a4067b8dd829598dad9e3660b2cd4
Michael Lynch <git@mtlynch.io>
2019-08-15 19:14:08 -0400

    Putting CSRF handling in proper files

--- handlers/cors_dev.go
+++ handlers/cors_dev.go
@@ -4,16 +4,13 @@ package handlers
 
 import (
 	"net/http"
-
-	"github.com/gorilla/csrf"
 )
 
 func (s defaultServer) enableCors(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://10.0.75.1:8081")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
-		w.Header().Set("X-CSRF-Token", csrf.Token(r))
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		h(w, r)
 	}
 }
--- handlers/csrf_dev.go
+++ handlers/csrf_dev.go
@@ -4,8 +4,13 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/gorilla/csrf"
 )
 
 func (s defaultServer) enableCsrf(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {}
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-CSRF-Token", csrf.Token(r))
+		h(w, r)
+	}
 }
