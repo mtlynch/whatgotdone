c0dde32bbb8c52e7e0d7d6f2e75514bd74bbc967
Michael Lynch <mtlynch@users.noreply.github.com>
2019-08-22 15:28:46 -0500

    Adding X-Csrf-Token to CORS headers (#249)

--- backend/handlers/cors_dev.go
+++ backend/handlers/cors_dev.go
@@ -10,7 +10,7 @@ func (s defaultServer) enableCors(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://10.0.75.1:8081")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Csrf-Token")
 		h(w, r)
 	}
 }
