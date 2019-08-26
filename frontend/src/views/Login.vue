672111fdb4e5eb0ee89f12bbdf6c38ce332debb9
Michael Lynch <git@mtlynch.io>
2019-08-26 12:28:07 -0400

    Switching to cleaner UserKit login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -10,11 +10,6 @@ import updateLoginState from "../controllers/LoginState.js";
 
 export default {
   name: "Login",
-  data() {
-    return {
-      polling: null
-    };
-  },
   mounted() {
     let userKitScript = document.createElement("script");
     userKitScript.setAttribute("src", "https://widget.userkit.io/widget.js");
@@ -26,26 +21,11 @@ export default {
     userKitScript.setAttribute("data-login-dismiss", "false");
     document.head.appendChild(userKitScript);
   },
-  beforeDestroy() {
-    clearInterval(this.polling);
-  },
   created() {
-    this.pollLoginStatus();
-  },
-  methods: {
-    pollLoginStatus() {
-      this.polling = setInterval(() => {
-        if (this.isLoggedIn()) {
-          clearInterval(this.polling);
-          updateLoginState(5);
-          this.$router.push("/entry/edit/" + thisFriday());
-        }
-      }, 100);
-    },
-    isLoggedIn() {
-      // eslint-disable-next-line
-      return typeof UserKit !== "undefined" && UserKit.isLoggedIn();
-    }
+    document.addEventListener("UserKitSignIn", () => {
+      updateLoginState(/*attempts=*/ 5);
+      this.$router.push("/entry/edit/" + thisFriday());
+    });
   }
 };
 </script>
