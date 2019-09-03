be5cead3765094942a6160f5745b267ddc53d5a5
Michael Lynch <git@mtlynch.io>
2019-09-03 11:24:02 -0400

    Revert "Temporarily disabling CSRF mitigation (#266)"
    This reverts commit d22af9b2d912b6da8d6857f521f31a01720821e7.

--- backend/handlers/csrf_prod.go
+++ backend/handlers/csrf_prod.go
@@ -12,8 +12,6 @@ func (s defaultServer) enableCsrf(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-CSRF-Token", csrf.Token(r))
 
-		// Temporarily disable CSRF protection until I figure out what's broken.
-		h(w, r) // TODO(mtlynch): Remove this and uncomment next line.
-		//s.csrfMiddleware(h).ServeHTTP(w, r)
+		s.csrfMiddleware(h).ServeHTTP(w, r)
 	}
 }
