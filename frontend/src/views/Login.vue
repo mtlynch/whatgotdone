d57930c0086275d1e905ef50e3b6ef40a93300cb
Michael Lynch <mtlynch@users.noreply.github.com>
2019-09-03 10:42:36 -0500

    Revert "Switching to cleaner UserKit login (#257)" (#269)
    This reverts commit 34dbc5dece1b702c2ab9befbc4ae974a8c5589d9.
    
    This change seems to cause the e2e test to become flaky.

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -10,6 +10,11 @@ import updateLoginState from "../controllers/LoginState.js";
 
 export default {
   name: "Login",
+  data() {
+    return {
+      polling: null
+    };
+  },
   mounted() {
     let userKitScript = document.createElement("script");
     userKitScript.setAttribute("src", "https://widget.userkit.io/widget.js");
@@ -21,11 +26,26 @@ export default {
     userKitScript.setAttribute("data-login-dismiss", "false");
     document.head.appendChild(userKitScript);
   },
+  beforeDestroy() {
+    clearInterval(this.polling);
+  },
   created() {
-    document.addEventListener("UserKitSignIn", () => {
-      updateLoginState(/*attempts=*/ 5);
-      this.$router.push("/entry/edit/" + thisFriday());
-    });
+    this.pollLoginStatus();
+  },
+  methods: {
+    pollLoginStatus() {
+      this.polling = setInterval(() => {
+        if (this.isLoggedIn()) {
+          clearInterval(this.polling);
+          updateLoginState(5);
+          this.$router.push("/entry/edit/" + thisFriday());
+        }
+      }, 100);
+    },
+    isLoggedIn() {
+      // eslint-disable-next-line
+      return typeof UserKit !== "undefined" && UserKit.isLoggedIn();
+    }
   }
 };
 </script>
