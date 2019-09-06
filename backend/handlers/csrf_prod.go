8378cc96254a6baaf24d6315261aeba1b3819648
Michael Lynch <mtlynch@users.noreply.github.com>
2019-09-06 16:52:57 -0500

    Re-disabling CSRF (#270)

--- backend/handlers/csrf_prod.go
+++ backend/handlers/csrf_prod.go
@@ -12,6 +12,8 @@ func (s defaultServer) enableCsrf(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-CSRF-Token", csrf.Token(r))
 
-		s.csrfMiddleware(h).ServeHTTP(w, r)
+		// Temporarily disable CSRF protection until I figure out what's broken.
+		h(w, r) // TODO(mtlynch): Remove this and uncomment next line.
+		//s.csrfMiddleware(h).ServeHTTP(w, r)
 	}
 }
