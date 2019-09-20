ef90cec9bf8c26f66dc6f792b3b71ea8fd2034f6
Michael Lynch <mtlynch@users.noreply.github.com>
2019-09-20 10:44:20 -0500

    Removing CSP policies for mailing list (#294)
    * Removing CSP policies for mailing list
    
    We no longer have a mailing list form, so this is not necessary.
    
    * Fixing CSP header

--- backend/handlers/csp_prod.go
+++ backend/handlers/csp_prod.go
@@ -29,12 +29,7 @@ func (s defaultServer) enableCsp(h http.HandlerFunc) http.HandlerFunc {
 			// For Google Analytics
 			"https://www.google-analytics.com",
 		}, " ")
-		frameSrc := strings.Join([]string{
-			"'self'",
-			// For sendinblue mailing list signup
-			"https://sibforms.com",
-		}, " ")
-		w.Header().Set("Content-Security-Policy", fmt.Sprintf("default-src %s; img-src %s; frame-src %s", defaultSrc, imgSrc, frameSrc))
+		w.Header().Set("Content-Security-Policy", fmt.Sprintf("default-src %s; img-src %s", defaultSrc, imgSrc))
 		h(w, r)
 	}
 }
