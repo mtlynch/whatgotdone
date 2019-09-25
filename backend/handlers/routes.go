af3e38040604ee62652d0c195c0c839a7c39dab8
Skyler <Skyler.Dickey@ahs.com>
2019-09-25 12:21:15 -0500

    Uses static router

--- backend/handlers/routes.go
+++ backend/handlers/routes.go
@@ -4,44 +4,48 @@ import (
 	"net/http"
 )
 
-func (s *defaultServer) routes() {
-	// Serve static resources.
-	fs := http.FileServer(http.Dir("./frontend/dist"))
-	s.router.PathPrefix("/js").Handler(fs)
-	s.router.PathPrefix("/css").Handler(fs)
-	s.router.PathPrefix("/images").Handler(fs)
-	s.router.Path("/android-chrome-192x192.png").Handler(fs)
-	s.router.Path("/android-chrome-512x512.png").Handler(fs)
-	s.router.Path("/app.js").Handler(fs)
-	s.router.Path("/apple-touch-icon.png").Handler(fs)
-	s.router.Path("/browserconfig.xml").Handler(fs)
-	s.router.Path("/favicon-16x16.png").Handler(fs)
-	s.router.Path("/favicon-32x32.png").Handler(fs)
-	s.router.Path("/favicon.ico").Handler(fs)
-	s.router.Path("/mstile-150x150.png").Handler(fs)
-	s.router.Path("/site.webmanifest").Handler(fs)
-
-	// Handle routes that require backend logic.
+// addAPI will add the api routes that require backend logic.
+func (s *defaultServer) addAPI() {
+	// API:entries
 	s.router.HandleFunc("/api/entries/{username}", s.enableCors(s.entriesGet())).Methods(http.MethodGet)
+	s.router.HandleFunc("/api/recentEntries", s.enableCors(s.recentEntriesGet())).Methods(http.MethodGet)
+	// API:draft
 	s.router.HandleFunc("/api/draft/{date}", s.enableCors(s.draftOptions())).Methods(http.MethodOptions)
 	s.router.HandleFunc("/api/draft/{date}", s.enableCors(s.draftGet())).Methods(http.MethodGet)
 	s.router.HandleFunc("/api/draft/{date}", s.enableCsrf(s.enableCors(s.draftPost()))).Methods(http.MethodPost)
+	// API:reactions
 	s.router.HandleFunc("/api/reactions/entry/{username}/{date}", s.enableCors(s.reactionsOptions())).Methods(http.MethodOptions)
 	s.router.HandleFunc("/api/reactions/entry/{username}/{date}", s.enableCors(s.reactionsGet())).Methods(http.MethodGet)
 	s.router.HandleFunc("/api/reactions/entry/{username}/{date}", s.enableCsrf(s.enableCors(s.reactionsPost()))).Methods(http.MethodPost)
-	s.router.HandleFunc("/api/recentEntries", s.enableCors(s.recentEntriesGet())).Methods(http.MethodGet)
-	s.router.HandleFunc("/api/user/me", s.enableCors(s.userMeGet())).Methods(http.MethodGet)
+	// API:submit
 	s.router.HandleFunc("/api/submit", s.enableCors(s.submitOptions())).Methods(http.MethodOptions)
 	s.router.HandleFunc("/api/submit", s.enableCsrf(s.enableCors(s.submitPost()))).Methods(http.MethodPost)
+	// API:user
+	s.router.HandleFunc("/api/user/me", s.enableCors(s.userMeGet())).Methods(http.MethodGet)
 	s.router.HandleFunc("/api/logout", s.enableCors(s.logoutOptions())).Methods(http.MethodOptions)
 	s.router.HandleFunc("/api/logout", s.enableCsrf(s.enableCors(s.logoutPost()))).Methods(http.MethodPost)
 	// Catchall for when no API route matches.
 	s.router.PathPrefix("/api").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid API path", http.StatusBadRequest)
 	})
+}
 
-	// Serve index.html, the base page HTML before Vue rendering happens, and
+// addStatic will add the static management
+func (s *defaultServer) addStatic() {
+	// TODO make this location configurable in the future with flags or env.
+	fs := http.FileServer(staticSystem{http.Dir(http.Dir("./frontend/dist"))})
+
+	s.router.
+		PathPrefix("/").
+		HandlerFunc(s.enableCsrf(s.enableCsp(fs.ServeHTTP))).
+		Methods(http.MethodGet)
+}
+
+func (s *defaultServer) routes() {
+	// Add static file management
+	s.addStatic()
+	// Add the API
+	s.addAPI()
 	// render certain page elements server-side.
 	s.router.PathPrefix("/{username}/{date}").HandlerFunc(s.enableCsrf(s.enableCsp(s.indexHandler()))).Methods(http.MethodGet)
-	s.router.PathPrefix("/").HandlerFunc(s.enableCsrf(s.enableCsp(s.indexHandler()))).Methods(http.MethodGet)
 }
---
+++ backend/handlers/static.go
@@ -0,0 +1,29 @@
+package handlers
+
+import (
+	"net/http"
+	"strings"
+)
+
+// staticSystem custom file system handler
+type staticSystem struct {
+	fs http.FileSystem
+}
+
+// Open opens file
+func (fs staticSystem) Open(path string) (http.File, error) {
+	f, err := fs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := fs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
