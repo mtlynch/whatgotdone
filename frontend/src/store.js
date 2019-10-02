61fb772fa21b27569eae355d43d2b0761e9ef499
Jeff Duke <Jeff.Duke@Gmail.com>
2019-10-02 11:07:04 -0400

    Unit Testing Setup (#313)
    * add jest vue plugin
    
    * move tests out to exported function to make testable
    
    * add basic tests for vuex mutations
    
    * add test for view component

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,28 +1,30 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VuexPersistence from 'vuex-persist'
+import VuexPersistence from 'vuex-persist';
 
 Vue.use(Vuex);
 
 const vuexLocal = new VuexPersistence({
-  storage: window.localStorage
-})
+  storage: window.localStorage,
+});
+
+export const mutations = {
+  setUsername(state, username) {
+    state.username = username;
+  },
+  clearUsername(state) {
+    state.username = null;
+  },
+  setRecent(state, entries) {
+    state.recentEntries = entries;
+  },
+};
 
 export default new Vuex.Store({
   state: {
     username: null,
-    recentEntries: null
+    recentEntries: null,
   },
-  mutations: {
-    setUsername(state, username) {
-      state.username = username
-    },
-    clearUsername(state) {
-      state.username = null;
-    },
-    setRecent(state, entries) {
-      state.recentEntries = entries;
-    },
-  },
-  plugins: [vuexLocal.plugin]
+  mutations,
+  plugins: [vuexLocal.plugin],
 });
