16dc24ae95dc8842239d11212cf8331b864301ff
Michael Lynch <git@mtlynch.io>
2019-10-18 23:41:34 -0400

    Starting to add user profile feature

---
+++ frontend/src/views/EditUserProfile.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Edit Profile</h1>
+
+    <p>Avatar: <input type="text" :value="avatarUrl" /></p>
+
+    <h2>About</h2>
+
+    <textarea>{{ aboutMarkdown }}</textarea>
+
+    <h2>Contact</h2>
+
+    <ul>
+      <li>Twitter: <input type="text" :value="twitterUsername" /></li>
+      <li>Email: <input type="text" :value="email" /></li>
+    </ul>
+
+    <b-button
+      to="/michael"
+      variant="primary"
+      class="float-right"
+    >Save</b-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "EditUserProfile",
+  data() {
+    return {
+      // TODO(mtlynch): Retrieve this from the server.
+      avatarUrl: "https://i.stack.imgur.com/rdgMY.jpg",
+      aboutMarkdown: "Hi, I'm Michael, creator of What Got Done.\n\nI also blog at [mtlynch.io](https://mtlynch.io).",
+      twitterUsername: "deliberatecoder",
+      email: "michael@mtlynch.io",
+      canEdit: true,
+    };
+  }
+};
+</script>
+
+<style scoped>
+* {
+  text-align: left;
+}
+</style>
---
+++ frontend/src/views/UserProfile.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <img :src="avatarUrl" />
+
+    <h1>{{ username }}</h1>
+
+    <p>Member since: {{ joinDate }}</p>
+    <p>Updates: {{ entryCount }}</p>
+
+    <h2>About</h2>
+
+    <vue-markdown
+      :linkify="false"
+      :html="false"
+      :anchorAttributes="{rel: 'ugc' }"
+      :source="aboutMarkdown"
+    ></vue-markdown>
+
+    <h2>Contact</h2>
+
+    <ul>
+      <li><a :href="'https://twitter.com/' + twitterUsername">@{{ twitterUsername }}</a> (Twitter)</li>
+      <li><a :href="'mailto:' + email">{{ email }}</a> (Email)</li>
+    </ul>
+
+    <b-button
+      v-if="canEdit"
+      to="/profile/edit/"
+      variant="primary"
+      class="edit-btn float-right"
+    >Edit</b-button>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueMarkdown from "vue-markdown";
+
+Vue.use(VueMarkdown);
+
+export default {
+  name: "UserProfile",
+  data() {
+    return {
+      // TODO(mtlynch): Retrieve this from the server.
+      avatarUrl: "https://i.stack.imgur.com/rdgMY.jpg",
+      username: "michael",
+      joinDate: "2019-03-01",
+      entryCount: 26,
+      aboutMarkdown: "Hi, I'm Michael, creator of What Got Done.\n\nI also blog at [mtlynch.io](https://mtlynch.io).",
+      twitterUsername: "deliberatecoder",
+      email: "michael@mtlynch.io",
+      canEdit: true,
+    };
+  },
+  components: {
+    VueMarkdown
+  }
+};
+</script>
+
+<style scoped>
+* {
+  text-align: left;
+}
+</style>
