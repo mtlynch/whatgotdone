d4dc6d04e654685c023c881e14eb30566257c60e
Michael Lynch <git@mtlynch.io>
2019-10-24 15:29:43 -0400

    Adding more flexible CORS settings for development

--- backend/handlers/cors_dev.go
+++ backend/handlers/cors_dev.go
@@ -7,12 +7,16 @@ import (
 )
 
 // enableCors sets the CORS headers so that when we run the frontend in dev
-// mode (on localhost:8085), it can still communicate with the backend server.
+// mode, it can still communicate with the backend server.
 func (s defaultServer) enableCors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8085")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Csrf-Token")
-			h.ServeHTTP(w, r)
-		})
+		if r.Header.Get("Origin") == "" {
+			http.Error(w, "Cross domain requests require Origin header", http.StatusBadRequest)
+			return
+		}
+		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Csrf-Token")
+		h.ServeHTTP(w, r)
+	})
 }
