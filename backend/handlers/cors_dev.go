3b8b03b531e4c54e136417d75473664f2a3487a0
Eduardo Díaz <edjroz@users.noreply.github.com>
2019-10-23 22:19:18 -0400

    refactor to use enableCors as middleware (#344)

--- backend/handlers/cors_dev.go
+++ backend/handlers/cors_dev.go
@@ -8,11 +8,11 @@ import (
 
 // enableCors sets the CORS headers so that when we run the frontend in dev
 // mode (on localhost:8085), it can still communicate with the backend server.
-func (s defaultServer) enableCors(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8085")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Csrf-Token")
-		h(w, r)
-	}
+func (s defaultServer) enableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8085")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Csrf-Token")
+			h.ServeHTTP(w, r)
+		})
 }
--- backend/handlers/cors_prod.go
+++ backend/handlers/cors_prod.go
@@ -5,8 +5,8 @@ package handlers
 import "net/http"
 
 // enableCors on production builds is a passthrough that does nothing.
-func (s defaultServer) enableCors(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		h(w, r)
-	}
+func (s defaultServer) enableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.ServeHTTP(w, r)
+	})
 }
