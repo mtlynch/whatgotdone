c19c09d488ef4c2d549fd693589285fb05b7285a
Skyler <Skyler.Dickey@ahs.com>
2019-10-28 13:00:03 -0500

    Group Logic

--- backend/handlers/hsts_prod.go
+++ backend/handlers/hsts_prod.go
@@ -7,7 +7,7 @@ import "net/http"
 func (s *defaultServer) hstsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		if r.URL.Scheme == "https" || // If the url tells us https
-			r.TLS != nil && r.TLS.HandshakeComplete { // if the request is using tls
+			(r.TLS != nil && r.TLS.HandshakeComplete) { // if the request is using tls
 
 			rw.Header().Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 
