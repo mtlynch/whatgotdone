09a96a0db79f5c415eb3e57da496cf1327db5ccc
Michael Lynch <git@mtlynch.io>
2019-10-29 13:10:13 -0400

    Clarifying docstring

--- backend/handlers/csrf_dev.go
+++ backend/handlers/csrf_dev.go
@@ -15,6 +15,8 @@ func getCsrfSeed() string {
 
 func (s defaultServer) enableCsrf(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// In dev mode, set the X-CSRF-Token on responses, but don't require
+		// requests to send a correct CSRF token back.
 		w.Header().Set("X-CSRF-Token", csrf.Token(r))
 		h.ServeHTTP(w, r)
 	})
