5cc842bbb96b2c5548d48910ba7a58e00caafdb7
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-14 16:38:46 -0600

    Upgrading frontend packages (#383)
    * Upgrading frontend packages
    
    * Fixing pagination
    
    * Removing unused import

--- frontend/src/views/ViewEntry.vue
+++ frontend/src/views/ViewEntry.vue
@@ -46,15 +46,11 @@
 </template>
 
 <script>
-import Vue from 'vue';
 import moment from 'moment';
 import Journal from '../components/Journal.vue';
 import Reactions from '../components/Reactions.vue';
 import Username from '../components/Username.vue';
-import Pagination from 'bootstrap-vue/es/components/pagination';
 import {thisFriday} from '../controllers/EntryDates.js';
-
-Vue.use(Pagination);
 
 export default {
   name: 'ViewEntry',
