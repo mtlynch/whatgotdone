71b7cf39f77d29e69c655e401d441670098bf253
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-14 15:04:31 -0600

    Disabling removeAttributeQuotes from webpack (#381)
    * Disabling removeAttributeQuotes from webpack
    
    * Only adjusting minify options when they're present

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,5 +2,23 @@ module.exports = {
   runtimeCompiler: true,
   devServer: {
     disableHostCheck: true,
+  },
+  // We have to disable removeAttributeQuotes otherwise webpack will
+  // prematurely remove quotes from things that actually need them once go
+  // populates template variables like:
+  //
+  //    content="[[.Title]]"
+  //
+  chainWebpack: config => {
+    config
+      .plugin('html').init((Plugin, args) => {
+        const newArgs = {
+            ...args[0],
+        };
+        if (newArgs.minify) {
+          newArgs.minify.removeAttributeQuotes = false;
+        }
+        return new Plugin(newArgs);
+    });
   }
 }
