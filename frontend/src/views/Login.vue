d91ea80b4f873b34719cab79917a554083ac9731
Michael Lynch <git@mtlynch.io>
2020-02-14 20:35:22 -0500

    Switching post-login to visit previous page rather than assume edit
    Now that there are other things a user might want to do after logging in, we should stop assuming they want to edit their latest entry

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,7 +5,6 @@
 </template>
 
 <script>
-import {thisFriday} from '../controllers/EntryDates.js';
 import updateLoginState from '../controllers/LoginState.js';
 import loadUserKit from '../controllers/UserKit.js';
 
@@ -16,14 +15,14 @@ export default {
       process.env.VUE_APP_USERKIT_APP_ID,
       (userKit, userKitWidget) => {
         if (userKit.isLoggedIn() === true) {
-          this.$router.replace('/entry/edit/' + thisFriday());
+          this.$router.back();
         } else {
           userKitWidget.open('login');
         }
       },
       () => {
         updateLoginState(/*attempts=*/ 5, () => {
-          this.$router.replace('/entry/edit/' + thisFriday());
+          this.$router.back();
         });
       }
     );
