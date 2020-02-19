aa54a4740edca995afb4168dce432129130414af
Jagadesh <jagadesh@hostmasterzone.com>
2020-02-19 15:05:28 -0500

    Disabled "More Entries" button when get request is in progress (#448)
    * Prevent dupes when user clicks "More entries" #430
    
    * Formatted code

--- frontend/src/views/Recent.vue
+++ frontend/src/views/Recent.vue
@@ -9,7 +9,10 @@
       v-for="item in recentEntries"
     />
 
-    <b-button variant="secondary" v-on:click="onLoadMore"
+    <b-button
+      variant="secondary"
+      v-bind:disabled="requestInProgress"
+      v-on:click="onLoadMore"
       >More Entries</b-button
     >
   </div>
@@ -25,6 +28,11 @@ export default {
   components: {
     PartialJournal,
   },
+  data() {
+    return {
+      requestInProgress: false,
+    };
+  },
   computed: {
     recentEntries() {
       return this.$store.state.recentEntries;
@@ -32,7 +40,13 @@ export default {
   },
   methods: {
     onLoadMore() {
-      return extendRecent();
+      /**
+       * To prevent muliple click, disable button while api call is in progress
+       */
+      this.requestInProgress = true; // Set the flag true before fetch call
+      return extendRecent(() => {
+        this.requestInProgress = false; // Reset the flag in callback
+      });
     },
   },
   created() {
