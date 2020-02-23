873f6810a3365fd72a919866c68d6446421b8df8
Michael Lynch <mtlynch@users.noreply.github.com>
2020-02-23 17:37:03 -0600

    Switching recent entries API calls to promise-based semantics (#456)

--- frontend/src/controllers/Recent.js
+++ frontend/src/controllers/Recent.js
@@ -1,40 +1,39 @@
 import axios from 'axios';
-import store from '@/store.js';
 
 // Number of entries to request from the server on each request;
 const updateSize = 15;
 
-export function refreshRecent() {
-  getRecent(0, updateSize, recentEntries => {
-    store.commit('setRecent', recentEntries);
+export function getRecent(start) {
+  return new Promise(function(resolve, reject) {
+    const url = `${process.env.VUE_APP_BACKEND_URL}/api/recentEntries?start=${start}&limit=${updateSize}`;
+    axios
+      .get(url)
+      .then(result => {
+        // Transform each response data into entry object
+        const recentEntries = result.data.map(rawEntry => {
+          return processEntry(rawEntry);
+        });
+        resolve(recentEntries);
+      })
+      .catch(err => reject(err));
   });
 }
 
-export function extendRecent(callback) {
-  let recentEntries = store.state.recentEntries;
-  getRecent(recentEntries.length, updateSize, newEntries => {
-    // Extract keys from recentEntries array
-    const recentEntriesKeySet = getRecentEntriesKey(recentEntries);
-    // Loop through new entries and add to recentEntries store only if key doesn't exist
-    newEntries.forEach(newEntry => {
-      if (!recentEntriesKeySet.has(newEntry.key)) {
-        recentEntries.push(newEntry);
-      }
-    });
-    store.commit('setRecent', recentEntries);
-    callback();
+// Merges two arrays of entries so that there are no duplicate keys.
+export function mergeEntryArrays(a, b) {
+  let merged = Array.from(a);
+  let keySet = new Set();
+  a.forEach(entry => {
+    keySet.add(entry.key);
   });
-}
 
-export function getRecent(start, limit, callback) {
-  const url = `${process.env.VUE_APP_BACKEND_URL}/api/recentEntries?start=${start}&limit=${limit}`;
-  axios.get(url).then(result => {
-    // Transform each response data into entry object
-    const recentEntries = result.data.map(rawEntry => {
-      return processEntry(rawEntry);
-    });
-    callback(recentEntries);
+  b.forEach(newEntry => {
+    if (!keySet.has(newEntry.key)) {
+      merged.push(newEntry);
+    }
   });
+
+  return merged;
 }
 
 function processEntry(entry) {
@@ -45,15 +44,4 @@ function processEntry(entry) {
     date: new Date(entry.date),
     markdown: entry.markdown,
   };
-}
-
-//Loop through all entries and put entry key in Set
-function getRecentEntriesKey(recentEntries) {
-  const entriesKeySet = new Set();
-
-  recentEntries.forEach(recentEntry => {
-    entriesKeySet.add(recentEntry.key);
-  });
-
-  return entriesKeySet;
 }
