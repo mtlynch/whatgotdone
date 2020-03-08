3192147412df6c47d3b3e077f2a67e5b8761c336
Michael Lynch <mtlynch@users.noreply.github.com>
2020-03-08 17:54:48 -0500

    Refactoring draft API logic into a controller (#482)
    * Refactoring draft API logic into a controller
    
    * Fixing URL

---
+++ frontend/src/controllers/Drafts.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+import getCsrfToken from '@/controllers/CsrfToken.js';
+
+export function getDraft(entryDate) {
+  return new Promise(function(resolve, reject) {
+    const url = `${process.env.VUE_APP_BACKEND_URL}/api/draft/${entryDate}`;
+    axios
+      .get(url, {withCredentials: true})
+      .then(result => {
+        resolve(result.data.markdown);
+      })
+      .catch(error => {
+        if (error.response.status == 404) {
+          resolve('');
+        } else {
+          reject(error);
+        }
+      });
+  });
+}
+
+export function saveDraft(entryDate, entryContent) {
+  return new Promise(function(resolve, reject) {
+    const url = `${process.env.VUE_APP_BACKEND_URL}/api/draft/${entryDate}`;
+    axios
+      .post(
+        url,
+        {
+          entryContent: entryContent,
+        },
+        {withCredentials: true, headers: {'X-CSRF-Token': getCsrfToken()}}
+      )
+      .then(result => {
+        resolve(result.data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
