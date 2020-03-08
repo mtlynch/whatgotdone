208113d386dbdf28ed1d66886f2522ebcbbc9718
Michael Lynch <git@mtlynch.io>
2020-03-08 13:25:35 -0400

    Refactoring logout logic to a controller

---
+++ frontend/src/controllers/Logout.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+import store from '@/store.js';
+
+import getCsrfToken from '@/controllers/CsrfToken.js';
+import {logoutUserKit} from '@/controllers/UserKit.js';
+
+function deleteCookie(name) {
+  document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
+}
+
+export function logout() {
+  store.commit('clearLoginState');
+  return new Promise(function(resolve, reject) {
+    const url = `${process.env.VUE_APP_BACKEND_URL}/api/logout`;
+    axios
+      .post(url, {}, {headers: {'X-CSRF-Token': getCsrfToken()}})
+      .then(() => {
+        logoutUserKit();
+      })
+      .catch(error => {
+        reject(error);
+      })
+      .finally(() => {
+        // Logout can fail if CSRF goes out of state. In this case, still
+        // delete the CSRF cookie.
+        deleteCookie('csrf_base_v3');
+      });
+  });
+}
--- frontend/src/views/Logout.vue
+++ frontend/src/views/Logout.vue
@@ -5,30 +5,14 @@
 </template>
 
 <script>
-import getCsrfToken from '@/controllers/CsrfToken.js';
-import {logoutUserKit} from '@/controllers/UserKit.js';
+import {logout} from '@/controllers/Logout.js';
 
 export default {
   name: 'Logout',
   created() {
-    this.$store.commit('clearLoginState');
-    const url = `${process.env.VUE_APP_BACKEND_URL}/api/logout`;
-    this.$http
-      .post(url, {}, {headers: {'X-CSRF-Token': getCsrfToken()}})
-      .then(() => {
-        logoutUserKit();
-        window.location.href = '/';
-      })
-      .finally(() => {
-        // Logout can fail if CSRF goes out of state. In this case, still
-        // delete the CSRF cookie.
-        this.deleteCookie('csrf_base_v3');
-      });
-  },
-  methods: {
-    deleteCookie(name) {
-      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
-    },
+    logout().then(() => {
+      window.location.href = '/';
+    });
   },
 };
 </script>
