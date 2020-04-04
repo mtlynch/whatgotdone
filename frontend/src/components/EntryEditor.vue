28d7912a0f2bb2f687e0333c6f42648405c1747b
Michael Lynch <mtlynch@users.noreply.github.com>
2020-04-04 13:11:26 -0500

    Refactoring entry editor into its own component (#498)
    * Refactoring entry editor into its own component
    
    * Updating contents on value change

---
+++ frontend/src/components/EntryEditor.vue
@@ -0,0 +1,37 @@
+<template>
+  <textarea-autosize
+    v-model="contents"
+    :min-height="250"
+    :max-height="650"
+    @input="onInput"
+  ></textarea-autosize>
+</template>
+
+<script>
+import Vue from 'vue';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+
+Vue.use(VueTextareaAutosize);
+
+export default {
+  name: 'EntryEditor',
+  props: {
+    value: String,
+  },
+  data() {
+    return {
+      contents: this.value,
+    };
+  },
+  methods: {
+    onInput(newValue) {
+      this.$emit('input', newValue);
+    },
+  },
+  watch: {
+    value: function(newValue) {
+      this.contents = newValue;
+    },
+  },
+};
+</script>
