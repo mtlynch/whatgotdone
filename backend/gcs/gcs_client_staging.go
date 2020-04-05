bb13f565ddfafb7c8c8608100166f3f00a1c2131
Michael Lynch <git@mtlynch.io>
2020-04-05 01:28:52 -0400

    Trimming implementation for staging GCS client

--- backend/gcs/gcs_client_staging.go
+++ backend/gcs/gcs_client_staging.go
@@ -4,17 +4,11 @@ package gcs
 
 import (
 	"context"
+	"errors"
 
 	"cloud.google.com/go/storage"
-	"google.golang.org/api/option"
 )
 
-func newGcsClient(ctx context.Context) (*storage.Client, error) {
-	const devServiceAccount = "service-account-creds-staging.json"
-	client, err := storage.NewClient(ctx, option.WithCredentialsFile(devServiceAccount))
-	if err != nil {
-		return nil, err
-	}
-
-	return client, nil
+func newGcsClient(_ context.Context) (*storage.Client, error) {
+	return nil, errors.New("staging environment does not support GCS client")
 }
