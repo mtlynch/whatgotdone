92dc2b08d86b0d7cee4e7fc1d42e3dc6d2afc307
Michael Lynch <git@mtlynch.io>
2020-10-04 22:14:56 -0400

    Remove duplicate integration test for entry drafts

--- integration/cypress/integration/entry_draft_spec.js
+++ integration/cypress/integration/entry_draft_spec.js
@@ -47,35 +47,6 @@ it("don't overwrite draft until we successfully sync the latest draft from the s
   cy.get(".save-draft").should("not.be.visible");
 });
 
-it("logs in and saves a draft", () => {
-  cy.server();
-  cy.route("GET", "/api/draft/*").as("getDraft");
-  cy.route("POST", "/api/draft/*").as("postDraft");
-
-  cy.login("staging_jimmy");
-
-  cy.location("pathname").should("include", "/entry/edit");
-
-  // Wait for page to pull down any previous entry.
-  cy.wait("@getDraft");
-
-  const entryText = "Saved a private draft at " + new Date().toISOString();
-
-  cy.get(".journal-markdown").clear().type(entryText);
-  cy.get(".save-draft").click();
-
-  // Wait for "save draft" operation to complete.
-  cy.wait("@postDraft");
-
-  // User should stay on the same page after saving a draft.
-  cy.location("pathname").should("include", "/entry/edit");
-
-  cy.visit("/recent");
-
-  // Private drafts should not appear on the recent page
-  cy.get("#app").should("not.contain", entryText);
-});
-
 it("uses the entry template for new drafts", () => {
   cy.server();
   cy.route("GET", "/api/draft/*").as("getDraft");
