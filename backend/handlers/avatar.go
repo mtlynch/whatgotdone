4fcee3d9e94b1fd49c5a07d93391f2030c0bc19e
Michael Lynch <git@mtlynch.io>
2021-05-15 14:00:11 -0400

    Add a partial PUT handler

--- backend/handlers/avatar.go
+++ backend/handlers/avatar.go
@@ -1,53 +1,57 @@
 package handlers
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
-func (s defaultServer) userAvatarFullGet() http.HandlerFunc {
+func (s *defaultServer) userAvatarPut() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		username, err := usernameFromRequestPath(r)
-		if err != nil {
-			log.Printf("failed to retrieve username from request path: %s", err)
-			http.Error(w, "Invalid username", http.StatusBadRequest)
+		if s.gcsClient == nil {
+			log.Printf("can't accept avatar upload because media uploads are disabled")
+			http.Error(w, fmt.Sprintf("User profile photo uploading is disabled"), http.StatusBadRequest)
 			return
 		}
-		// TODO: Replace placeholder with this
-		url := fmt.Sprintf("https://storage.googleapis.com/%s/avatars/%s/full-300px.jpg", "whatgotdone-staging", username)
 
-		if username == "testjoe" {
-			url = "https://placekitten.com/300/300"
+		username, err := s.loggedInUser(r)
+		if err != nil {
+			http.Error(w, "You must be logged in to upload a profile photo", http.StatusForbidden)
+			return
 		}
 
-		http.Redirect(
-			w,
-			r,
-			url,
-			http.StatusTemporaryRedirect)
+		avatarFile, contentType, err := avatarFileFromRequest(w, r)
+		if err != nil {
+			log.Printf("failed to read media from request: %v", err)
+			http.Error(w, fmt.Sprintf("Media upload failed: %v", err), http.StatusBadRequest)
+			return
+		}
+
+		path := fmt.Sprintf("/avatars/%s/%s-avatar.jpg", username, username)
+
+		_, err = s.gcsClient.UploadFile(avatarFile, path, contentType)
+		if err != nil {
+			log.Printf("failed to upload avatar to static storage: %v", err)
+			http.Error(w, fmt.Sprintf("Profile photo upload failed: %v", err), http.StatusBadRequest)
+			return
+		}
 	}
 }
 
-func (s defaultServer) userAvatarThumbnailGet() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		username, err := usernameFromRequestPath(r)
-		if err != nil {
-			log.Printf("failed to retrieve username from request path: %s", err)
-			http.Error(w, "Invalid username", http.StatusBadRequest)
-			return
-		}
-		// TODO: Replace placeholder with this
-		url := fmt.Sprintf("https://storage.googleapis.com/%s/avatars/%s/thumb-30px.jpg", "whatgotdone-staging", username)
+const maxAvatarBytes = 8 * 1024 * 1024 // 8 MB
 
-		if username == "testjoe" {
-			url = "https://placekitten.com/300/300"
-		}
-
-		http.Redirect(
-			w,
-			r,
-			url,
-			http.StatusTemporaryRedirect)
+func avatarFileFromRequest(w http.ResponseWriter, r *http.Request) (io.Reader, string, error) {
+	r.Body = http.MaxBytesReader(w, r.Body, maxAvatarBytes)
+	r.ParseMultipartForm(32 << 20)
+	file, metadata, err := r.FormFile("file")
+	if err != nil {
+		return nil, "", err
 	}
+	contentType := metadata.Header.Get("Content-Type")
+	if contentType != "image/jpeg" && contentType != "image/png" {
+		return nil, "", errors.New("invalid avatar file format")
+	}
+	return file, contentType, nil
 }
