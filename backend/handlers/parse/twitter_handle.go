c50b6c3f38c9827f53a78de9567dddad64b783c5
Michael Lynch <git@mtlynch.io>
2021-06-12 13:48:01 -0400

    Parse twitter handles more rigorously

--- backend/handlers/parse/twitter_handle.go
+++ backend/handlers/parse/twitter_handle.go
@@ -7,11 +7,21 @@ import (
 	"github.com/mtlynch/whatgotdone/backend/types"
 )
 
+var (
+	InvalidTwitterHandleError = errors.New("invalid twitter handle")
+
+	twitterHandlePattern = regexp.MustCompile("^[A-Za-z0-9_]{4,15}$")
+)
+
 // TwitterHandle parses a raw string into a TwitterHandle, validating that it's
 // a legal Twitter handle.
 func TwitterHandle(handle string) (types.TwitterHandle, error) {
-	if !regexp.MustCompile("^[A-Za-z0-9_]{1,15}$").MatchString(handle) {
-		return types.TwitterHandle(""), errors.New("invalid Twitter handle")
+	if isUndefinedFromJavascript(handle) {
+		return types.TwitterHandle(""), InvalidTwitterHandleError
+	}
+
+	if !twitterHandlePattern.MatchString(handle) {
+		return types.TwitterHandle(""), InvalidTwitterHandleError
 	}
 	return types.TwitterHandle(handle), nil
 }
