493161914019dc243d65ae383880054b7fd9e02a
Michael Lynch <git@mtlynch.io>
2021-11-13 21:28:49 -0500

    Refactor preferences controllers

--- frontend/src/controllers/Media.js
+++ frontend/src/controllers/Media.js
@@ -1,25 +1,15 @@
-import axios from 'axios';
-
-import {getCsrfToken} from '@/controllers/Common.js';
+import {getCsrfToken, processJsonResponse} from '@/controllers/Common.js';
 
 export function uploadMedia(media) {
-  return new Promise(function (resolve, reject) {
-    let formData = new FormData();
-    formData.append('file', media);
-    let url = `${process.env.VUE_APP_BACKEND_URL}/api/media`;
-    axios
-      .put(url, formData, {
-        withCredentials: true,
-        headers: {
-          'X-CSRF-Token': getCsrfToken(),
-          'Content-Type': 'multipart/form-data',
-        },
-      })
-      .then((response) => {
-        resolve(response.data.url);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+  let formData = new FormData();
+  formData.append('file', media);
+  return fetch(`${process.env.VUE_APP_BACKEND_URL}/api/media`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'X-CSRF-Token': getCsrfToken(),
+      'Content-Type': 'multipart/form-data',
+    },
+    body: formData,
+  }).then(processJsonResponse);
 }
