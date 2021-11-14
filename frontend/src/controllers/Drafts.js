c622903000a38961cfc388f5d20f3ab18f626623
Michael Lynch <git@mtlynch.io>
2021-11-13 21:27:12 -0500

    Refactor drafts controller

--- frontend/src/controllers/Drafts.js
+++ frontend/src/controllers/Drafts.js
@@ -1,34 +1,31 @@
-import axios from 'axios';
-
-import {getCsrfToken} from '@/controllers/Common.js';
+import {getCsrfToken, processJsonResponse} from '@/controllers/Common.js';
 
 export function getDraft(entryDate) {
-  return new Promise(function (resolve, reject) {
-    const url = `${process.env.VUE_APP_BACKEND_URL}/api/draft/${entryDate}`;
-    axios
-      .get(url, {withCredentials: true})
-      .then((result) => {
-        if (!Object.prototype.hasOwnProperty.call(result, 'data')) {
-          return Promise.reject(
-            new Error('response is missing expected field: data')
-          );
-        }
-        if (!Object.prototype.hasOwnProperty.call(result.data, 'markdown')) {
-          return Promise.reject(
-            new Error('response is missing expected field: data.markdown')
-          );
-        }
-        resolve(result.data.markdown);
-      })
-      .catch((error) => {
-        // A 404 is not an error.
-        if (error?.response?.status === 404) {
-          resolve('');
-        } else {
-          reject(error);
-        }
-      });
-  });
+  return fetch(`${process.env.VUE_APP_BACKEND_URL}/api/draft/${entryDate}`, {
+    credentials: 'include',
+  })
+    .then(processJsonResponse)
+    .then((result) => {
+      if (!Object.prototype.hasOwnProperty.call(result, 'data')) {
+        return Promise.reject(
+          new Error('response is missing expected field: data')
+        );
+      }
+      if (!Object.prototype.hasOwnProperty.call(result.data, 'markdown')) {
+        return Promise.reject(
+          new Error('response is missing expected field: data.markdown')
+        );
+      }
+      Promise.resolve(result.data.markdown);
+    })
+    .catch((error) => {
+      // A 404 is not an error.
+      if (error?.response?.status === 404) {
+        Promise.resolve('');
+      } else {
+        Promise.reject(error);
+      }
+    });
 }
 
 export function saveDraft(entryDate, entryContent) {
