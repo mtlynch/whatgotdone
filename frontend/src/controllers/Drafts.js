92eb00c4f0cb56e7a6c4aa496c96f50ce071884e
Michael Lynch <mtlynch@users.noreply.github.com>
2021-11-13 21:22:04 -0500

    Change /draft/{date} and /entry/{date} routes from POST to PUT (#645)
    Re-reading the definition of PUT vs. POST, these make more sense as PUT.

--- frontend/src/controllers/Drafts.js
+++ frontend/src/controllers/Drafts.js
@@ -35,7 +35,7 @@ export function saveDraft(entryDate, entryContent) {
   return new Promise(function (resolve, reject) {
     const url = `${process.env.VUE_APP_BACKEND_URL}/api/draft/${entryDate}`;
     axios
-      .post(
+      .put(
         url,
         {
           entryContent: entryContent,
