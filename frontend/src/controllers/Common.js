eab2178aebe3e411ae3e477d6008736aab759800
Michael Lynch <mtlynch@users.noreply.github.com>
2021-11-13 21:37:21 -0500

    Refactor preferences controllers (#650)

--- frontend/src/controllers/Common.js
+++ frontend/src/controllers/Common.js
@@ -4,3 +4,12 @@ export function getCsrfToken() {
     .querySelector("meta[name='csrf-token']")
     .getAttribute('content');
 }
+
+export function processJsonResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  return response.text().then((error) => {
+    return Promise.reject(error);
+  });
+}
