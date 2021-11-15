ddbe088afe51ef671fc01f80e6c96a4d46420ee2
Michael Lynch <git@mtlynch.io>
2021-11-14 22:28:51 -0500

    Add separator before URLSearchParams

--- frontend/src/controllers/PageViews.js
+++ frontend/src/controllers/PageViews.js
@@ -2,7 +2,7 @@ import {processJsonResponse} from '@/controllers/Common.js';
 
 export function getPageViews(path) {
   return fetch(
-    `${process.env.VUE_APP_BACKEND_URL}/api/pageViews` +
+    `${process.env.VUE_APP_BACKEND_URL}/api/pageViews?` +
       new URLSearchParams({
         path,
       })
