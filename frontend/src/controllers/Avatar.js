321bb7082e5a46980860072882327bcf62bc0938
Michael Lynch <git@mtlynch.io>
2021-11-14 22:48:34 -0500

    Refactor avatar controllers

--- frontend/src/controllers/Avatar.js
+++ frontend/src/controllers/Avatar.js
@@ -1,42 +1,25 @@
-import axios from 'axios';
-
 import {getCsrfToken} from '@/controllers/Common.js';
 
 export function deleteAvatar() {
-  return new Promise(function (resolve, reject) {
-    axios
-      .delete(`${process.env.VUE_APP_BACKEND_URL}/api/user/avatar`, {
-        withCredentials: true,
-        headers: {
-          'X-CSRF-Token': getCsrfToken(),
-        },
-      })
-      .then(() => {
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
+  return fetch(`${process.env.VUE_APP_BACKEND_URL}/api/user/avatar`, {
+    method: 'DELETE',
+    credentials: 'include',
+    headers: {
+      'X-CSRF-Token': getCsrfToken(),
+    },
   });
 }
 
 export function uploadAvatar(image) {
-  return new Promise(function (resolve, reject) {
-    const formData = new FormData();
-    formData.append('file', image);
-    axios
-      .put(`${process.env.VUE_APP_BACKEND_URL}/api/user/avatar`, formData, {
-        withCredentials: true,
-        headers: {
-          'X-CSRF-Token': getCsrfToken(),
-          'Content-Type': 'multipart/form-data',
-        },
-      })
-      .then(() => {
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
+  const formData = new FormData();
+  formData.append('file', image);
+  return fetch(`${process.env.VUE_APP_BACKEND_URL}/api/user/avatar`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      'X-CSRF-Token': getCsrfToken(),
+      'Content-Type': 'multipart/form-data',
+    },
+    body: formData,
   });
 }
