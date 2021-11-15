7b9174e269a78a85cf8d375bb864fe8ed2ceef8d
Michael Lynch <git@mtlynch.io>
2021-11-14 22:40:34 -0500

    Refactor preferences controllers

--- frontend/src/controllers/Preferences.js
+++ frontend/src/controllers/Preferences.js
@@ -1,36 +1,16 @@
-import axios from 'axios';
-
-import {getCsrfToken} from '@/controllers/Common.js';
-
-const url = `${process.env.VUE_APP_BACKEND_URL}/api/preferences`;
+import {getCsrfToken, processJsonResult} from '@/controllers/Common.js';
 
 export function getPreferences() {
-  return new Promise(function (resolve, reject) {
-    axios
-      .get(url, {
-        withCredentials: true,
-      })
-      .then((result) => {
-        resolve(result.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  return fetch(`${process.env.VUE_APP_BACKEND_URL}/api/preferences`, {
+    credentials: 'include',
+  }).then(processJsonResult);
 }
 
 export function savePreferences(preferences) {
-  return new Promise(function (resolve, reject) {
-    axios
-      .put(url, preferences, {
-        withCredentials: true,
-        headers: {'X-CSRF-Token': getCsrfToken()},
-      })
-      .then(() => {
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
+  return fetch(`${process.env.VUE_APP_BACKEND_URL}/api/preferences`, {
+    method: 'PUT',
+    headers: {'X-CSRF-Token': getCsrfToken()},
+    credentials: 'include',
+    body: JSON.stringify(preferences),
   });
 }
