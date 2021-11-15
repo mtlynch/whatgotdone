8df7e40c0bc05d3cc035c67f7c51aab57d4ace32
Michael Lynch <mtlynch@users.noreply.github.com>
2021-11-14 22:41:48 -0500

    Refactor reactions controller (#656)

--- frontend/src/controllers/Reactions.js
+++ frontend/src/controllers/Reactions.js
@@ -1,37 +1,21 @@
-import axios from 'axios';
-
-import {getCsrfToken} from '@/controllers/Common.js';
+import {getCsrfToken, processJsonResponse} from '@/controllers/Common.js';
 
 export function getReactions(entryAuthor, entryDate) {
-  return new Promise(function (resolve, reject) {
-    const url = `${process.env.VUE_APP_BACKEND_URL}/api/reactions/entry/${entryAuthor}/${entryDate}`;
-    axios
-      .get(url)
-      .then((result) => {
-        resolve(result.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  return fetch(
+    `${process.env.VUE_APP_BACKEND_URL}/api/reactions/entry/${entryAuthor}/${entryDate}`
+  ).then(processJsonResponse);
 }
 
 export function setReaction(entryAuthor, entryDate, reaction) {
-  return new Promise(function (resolve, reject) {
-    const url = `${process.env.VUE_APP_BACKEND_URL}/api/reactions/entry/${entryAuthor}/${entryDate}`;
-    axios
-      .post(
-        url,
-        {
-          reactionSymbol: reaction,
-        },
-        {withCredentials: true, headers: {'X-CSRF-Token': getCsrfToken()}}
-      )
-      .then(() => {
-        resolve();
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  return fetch(
+    `${process.env.VUE_APP_BACKEND_URL}/api/reactions/entry/${entryAuthor}/${entryDate}`,
+    {
+      method: 'POST',
+      credentials: 'include',
+      headers: {'X-CSRF-Token': getCsrfToken()},
+      body: JSON.stringify({
+        reactionSymbol: reaction,
+      }),
+    }
+  );
 }
