2eeb73ef6da380c5e5f2cf9984fae0adf19529be
Michael Lynch <git@mtlynch.io>
2021-11-15 23:26:47 -0500

    Refactor entries controller

--- frontend/src/controllers/Entries.js
+++ frontend/src/controllers/Entries.js
@@ -1,52 +1,37 @@
-import axios from 'axios';
-
-import {getCsrfToken} from '@/controllers/Common.js';
+import {getCsrfToken, processJsonResponse} from '@/controllers/Common.js';
 
 export function getEntriesFromUser(username, project = null) {
-  return new Promise(function (resolve, reject) {
-    let url = `${process.env.VUE_APP_BACKEND_URL}/api/entries/${username}`;
-    if (project) {
-      url += `/project/${project}`;
-    }
-    axios
-      .get(url)
-      .then((result) => {
-        let entries = [];
-        for (const entry of result.data) {
-          entries.push({
-            permalink: `/${username}/${entry.date}`,
-            author: username,
-            date: new Date(entry.date),
-            lastModified: new Date(entry.lastModified),
-            markdown: entry.markdown,
-          });
-        }
-        // Sort newest to oldest.
-        entries.sort((a, b) => b.date - a.date);
-        resolve(entries);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  let url = `${process.env.VUE_APP_BACKEND_URL}/api/entries/${username}`;
+  if (project) {
+    url += `/project/${project}`;
+  }
+  return fetch(url)
+    .then(processJsonResponse)
+    .then((entriesRaw) => {
+      let entries = [];
+      for (const entry of entriesRaw) {
+        entries.push({
+          permalink: `/${username}/${entry.date}`,
+          author: username,
+          date: new Date(entry.date),
+          lastModified: new Date(entry.lastModified),
+          markdown: entry.markdown,
+        });
+      }
+      // Sort newest to oldest.
+      entries.sort((a, b) => b.date - a.date);
+      return Promise.resolve(entries);
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
 }
 
 export function saveEntry(entryDate, entryContent) {
-  return new Promise(function (resolve, reject) {
-    const url = `${process.env.VUE_APP_BACKEND_URL}/api/entry/${entryDate}`;
-    axios
-      .put(
-        url,
-        {
-          entryContent: entryContent,
-        },
-        {withCredentials: true, headers: {'X-CSRF-Token': getCsrfToken()}}
-      )
-      .then((result) => {
-        resolve(result.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  return fetch(`${process.env.VUE_APP_BACKEND_URL}/api/entry/${entryDate}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {'X-CSRF-Token': getCsrfToken()},
+    body: JSON.stringify({entryContent: entryContent}),
+  }).then(processJsonResponse);
 }
