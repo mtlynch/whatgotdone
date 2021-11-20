b07c4f2544043150e0c5251f7805a0e95e31fc99
Michael Lynch <git@mtlynch.io>
2021-11-19 22:15:50 -0500

    Refactor export

--- backend/handlers/export.go
+++ backend/handlers/export.go
@@ -8,68 +8,77 @@ import (
 	"github.com/mtlynch/whatgotdone/backend/types"
 )
 
-type exportedEntry struct {
-	Date         types.EntryDate `json:"date"`
-	Markdown     string          `json:"markdown"`
-	LastModified string          `json:"lastModified"`
-}
+type (
+	exportedEntry struct {
+		Date         types.EntryDate `json:"date"`
+		Markdown     string          `json:"markdown"`
+		LastModified string          `json:"lastModified"`
+	}
 
-type exportedPreferences struct {
-	EntryTemplate string `json:"entryTemplate"`
-}
+	exportedPreferences struct {
+		EntryTemplate string `json:"entryTemplate"`
+	}
+
+	exportedUserData struct {
+		Entries     []exportedEntry     `json:"entries"`
+		Drafts      []exportedEntry     `json:"drafts"`
+		Following   []types.Username    `json:"following"`
+		Profile     profilePublic       `json:"profile"`
+		Preferences exportedPreferences `json:"preferences"`
+	}
+)
 
 func (s defaultServer) exportGet() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		username, err := s.loggedInUser(r)
 		if err != nil {
-			http.Error(w, "You must log in to retrieve a draft entry", http.StatusForbidden)
+			http.Error(w, "You must log in to export your data", http.StatusForbidden)
 			return
 		}
 
-		// TODO: Get drafts
-
-		entries, err := s.datastore.GetEntries(username)
+		d, err := s.exportUserData(username)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("Failed to retrieve published entries: %s", err), http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("Failed to export user data: %s", err), http.StatusInternalServerError)
 			return
 		}
 
-		prefs, err := s.datastore.GetPreferences(username)
-		if _, ok := err.(datastore.PreferencesNotFoundError); ok {
-			prefs = types.Preferences{}
-		} else if err != nil {
-			http.Error(w, fmt.Sprintf("Failed to retrieve user preferences: %s", err), http.StatusInternalServerError)
-			return
-		}
-
-		profile, err := s.datastore.GetUserProfile(username)
-		if _, ok := err.(datastore.UserProfileNotFoundError); ok {
-			profile = types.UserProfile{}
-		} else if err != nil {
-			http.Error(w, fmt.Sprintf("Failed to retrieve user profile: %s", err), http.StatusInternalServerError)
-			return
-		}
-
-		following, err := s.datastore.Following(username)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Failed to retrieve following users: %s", err), http.StatusInternalServerError)
-			return
-		}
-
-		respondOK(w, struct {
-			Entries     []exportedEntry     `json:"entries"`
-			Drafts      []exportedEntry     `json:"drafts"`
-			Following   []types.Username    `json:"following"`
-			Profile     profilePublic       `json:"profile"`
-			Preferences exportedPreferences `json:"preferences"`
-		}{
-			Entries:   entriesToExportedEntries(entries, username),
-			Following: following,
-			Profile:   profileToPublic(profile),
-			Preferences: exportedPreferences{
-				EntryTemplate: prefs.EntryTemplate,
-			}})
+		respondOK(w, d)
 	}
+}
+
+func (s defaultServer) exportUserData(username types.Username) (exportedUserData, error) {
+	entries, err := s.datastore.GetEntries(username)
+	if err != nil {
+		return exportedUserData{}, err
+	}
+
+	prefs, err := s.datastore.GetPreferences(username)
+	if _, ok := err.(datastore.PreferencesNotFoundError); ok {
+		prefs = types.Preferences{}
+	} else if err != nil {
+		return exportedUserData{}, err
+	}
+
+	profile, err := s.datastore.GetUserProfile(username)
+	if _, ok := err.(datastore.UserProfileNotFoundError); ok {
+		profile = types.UserProfile{}
+	} else if err != nil {
+		return exportedUserData{}, err
+	}
+
+	following, err := s.datastore.Following(username)
+	if err != nil {
+		return exportedUserData{}, err
+	}
+
+	return exportedUserData{
+		Entries:   entriesToExportedEntries(entries, username),
+		Following: following,
+		Profile:   profileToPublic(profile),
+		Preferences: exportedPreferences{
+			EntryTemplate: prefs.EntryTemplate,
+		},
+	}, nil
 }
 
 func entriesToExportedEntries(entries []types.JournalEntry, author types.Username) []exportedEntry {
