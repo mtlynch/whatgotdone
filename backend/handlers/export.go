f5a02ed2cbeadfda948ed5b415d39749dd591129
Michael Lynch <mtlynch@users.noreply.github.com>
2021-11-20 15:12:25 -0500

    Parallelize exporting of draft entries (#678)

--- backend/handlers/export.go
+++ backend/handlers/export.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sort"
+	"sync"
 	"time"
 
 	"github.com/mtlynch/whatgotdone/backend/datastore"
@@ -52,16 +54,20 @@ func (s defaultServer) exportGet() http.HandlerFunc {
 
 func (s defaultServer) exportUserData(username types.Username) (exportedUserData, error) {
 	log.Printf("starting export for %s", username)
+
+	log.Printf("exporting(%s): unpublished drafts", username)
 	drafts, err := s.exportUserDrafts(username)
 	if err != nil {
 		return exportedUserData{}, err
 	}
 
+	log.Printf("exporting(%s): published entries", username)
 	entries, err := s.datastore.GetEntries(username)
 	if err != nil {
 		return exportedUserData{}, err
 	}
 
+	log.Printf("exporting(%s): preferences", username)
 	prefs, err := s.datastore.GetPreferences(username)
 	if _, ok := err.(datastore.PreferencesNotFoundError); ok {
 		prefs = types.Preferences{}
@@ -69,6 +75,7 @@ func (s defaultServer) exportUserData(username types.Username) (exportedUserData
 		return exportedUserData{}, err
 	}
 
+	log.Printf("exporting(%s): user profile", username)
 	profile, err := s.datastore.GetUserProfile(username)
 	if _, ok := err.(datastore.UserProfileNotFoundError); ok {
 		profile = types.UserProfile{}
@@ -76,6 +83,7 @@ func (s defaultServer) exportUserData(username types.Username) (exportedUserData
 		return exportedUserData{}, err
 	}
 
+	log.Printf("exporting(%s): followed users", username)
 	following, err := s.datastore.Following(username)
 	if err != nil {
 		return exportedUserData{}, err
@@ -95,11 +103,16 @@ func (s defaultServer) exportUserData(username types.Username) (exportedUserData
 }
 
 func (s defaultServer) exportUserDrafts(username types.Username) ([]types.JournalEntry, error) {
-	drafts := []types.JournalEntry{}
-
 	// Retrieve all the user's drafts by checking every possible draft date for an
 	// entry. This is inefficient, and we could optimize/parallelize this, but
 	// exporting isn't a very common or performance-sensitive code path.
+
+	type result struct {
+		draft types.JournalEntry
+		err   error
+	}
+	c := make(chan result)
+	var wg sync.WaitGroup
 
 	// 2019-03-29 is the first ever post on What Got Done.
 	currentDate := time.Date(2019, time.March, 29, 0, 0, 0, 0, time.UTC)
@@ -107,19 +120,43 @@ func (s defaultServer) exportUserDrafts(username types.Username) ([]types.Journa
 		if currentDate.After(dates.ThisFriday()) {
 			break
 		}
-		draft, err := s.datastore.GetDraft(username, types.EntryDate(currentDate.Format("2006-01-02")))
-		if _, ok := err.(datastore.DraftNotFoundError); ok {
-			// Ignore not found errors
-		} else if err != nil {
-			return []types.JournalEntry{}, err
-		}
-		if err == nil {
-			drafts = append(drafts, draft)
-		}
+		wg.Add(1)
+		go func(date types.EntryDate) {
+			defer wg.Done()
+			draft, err := s.datastore.GetDraft(username, date)
+			c <- result{draft, err}
+		}(types.EntryDate(currentDate.Format("2006-01-02")))
 
+		// Increment to next Friday.
 		currentDate = currentDate.AddDate(0, 0, 7)
 	}
 
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	drafts := []types.JournalEntry{}
+	var err error
+	for res := range c {
+		if res.err == nil {
+			drafts = append(drafts, res.draft)
+			continue
+		}
+		if _, ok := err.(datastore.DraftNotFoundError); ok {
+			continue
+		}
+		// Don't exit immediately because otherwise we'd leak the chan. Instead,
+		//  save the first error we encounter.
+		if err == nil && res.err != nil {
+			err = res.err
+		}
+	}
+
+	// Sort drafts in ascending order of date.
+	sort.Slice(drafts, func(i, j int) bool {
+		return drafts[i].Date < drafts[j].Date
+	})
 	return drafts, nil
 }
 
