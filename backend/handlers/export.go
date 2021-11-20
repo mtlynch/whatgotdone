195bdb799687eabe3d2fe5154cc8236bfa6a8013
Michael Lynch <mtlynch@users.noreply.github.com>
2021-11-20 14:11:11 -0500

    Add support for exporting user data as JSON (#668)
    * Add API for exporting user data as JSON
    
    Fixes #542
    
    * Export follow list, profile, and user preferences
    
    * Refactor export
    
    * Add export button to profile page
    
    * Fix name of export-data-btn
    
    * Add tests for export
    
    * Add comment

---
+++ backend/handlers/export.go
@@ -0,0 +1,133 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/mtlynch/whatgotdone/backend/datastore"
+	"github.com/mtlynch/whatgotdone/backend/dates"
+	"github.com/mtlynch/whatgotdone/backend/types"
+)
+
+type (
+	exportedEntry struct {
+		Date         types.EntryDate `json:"date"`
+		Markdown     string          `json:"markdown"`
+		LastModified string          `json:"lastModified"`
+	}
+
+	exportedPreferences struct {
+		EntryTemplate string `json:"entryTemplate"`
+	}
+
+	exportedUserData struct {
+		Entries     []exportedEntry     `json:"entries"`
+		Drafts      []exportedEntry     `json:"drafts"`
+		Following   []types.Username    `json:"following"`
+		Profile     profilePublic       `json:"profile"`
+		Preferences exportedPreferences `json:"preferences"`
+	}
+)
+
+func (s defaultServer) exportGet() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		username, err := s.loggedInUser(r)
+		if err != nil {
+			http.Error(w, "You must log in to export your data", http.StatusForbidden)
+			return
+		}
+
+		d, err := s.exportUserData(username)
+		if err != nil {
+			log.Printf("Failed to export user data: %v", err)
+			http.Error(w, fmt.Sprintf("Failed to export user data: %s", err), http.StatusInternalServerError)
+			return
+		}
+
+		respondOK(w, d)
+	}
+}
+
+func (s defaultServer) exportUserData(username types.Username) (exportedUserData, error) {
+	drafts, err := s.exportUserDrafts(username)
+	if err != nil {
+		return exportedUserData{}, err
+	}
+
+	entries, err := s.datastore.GetEntries(username)
+	if err != nil {
+		return exportedUserData{}, err
+	}
+
+	prefs, err := s.datastore.GetPreferences(username)
+	if _, ok := err.(datastore.PreferencesNotFoundError); ok {
+		prefs = types.Preferences{}
+	} else if err != nil {
+		return exportedUserData{}, err
+	}
+
+	profile, err := s.datastore.GetUserProfile(username)
+	if _, ok := err.(datastore.UserProfileNotFoundError); ok {
+		profile = types.UserProfile{}
+	} else if err != nil {
+		return exportedUserData{}, err
+	}
+
+	following, err := s.datastore.Following(username)
+	if err != nil {
+		return exportedUserData{}, err
+	}
+
+	return exportedUserData{
+		Entries:   entriesToExportedEntries(entries, username),
+		Drafts:    entriesToExportedEntries(drafts, username),
+		Following: following,
+		Profile:   profileToPublic(profile),
+		Preferences: exportedPreferences{
+			EntryTemplate: prefs.EntryTemplate,
+		},
+	}, nil
+}
+
+func (s defaultServer) exportUserDrafts(username types.Username) ([]types.JournalEntry, error) {
+	drafts := []types.JournalEntry{}
+
+	// Retrieve all the user's drafts by checking every possible draft date for an
+	// entry. This is inefficient, and we could optimize/parallelize this, but
+	// exporting isn't a very common or performance-sensitive code path.
+
+	// 2019-03-29 is the first ever post on What Got Done.
+	currentDate := time.Date(2019, time.March, 29, 0, 0, 0, 0, time.UTC)
+	for {
+		if currentDate.After(dates.ThisFriday()) {
+			break
+		}
+		draft, err := s.datastore.GetDraft(username, types.EntryDate(currentDate.Format("2006-01-02")))
+		if _, ok := err.(datastore.DraftNotFoundError); ok {
+			// Ignore not found errors
+		} else if err != nil {
+			return []types.JournalEntry{}, err
+		}
+		if err == nil {
+			drafts = append(drafts, draft)
+		}
+
+		currentDate = currentDate.AddDate(0, 0, 7)
+	}
+
+	return drafts, nil
+}
+
+func entriesToExportedEntries(entries []types.JournalEntry, author types.Username) []exportedEntry {
+	p := []exportedEntry{}
+	for _, entry := range entries {
+		p = append(p, exportedEntry{
+			Date:         entry.Date,
+			Markdown:     entry.Markdown,
+			LastModified: entry.LastModified,
+		})
+	}
+	return p
+}
---
+++ backend/handlers/export_test.go
@@ -0,0 +1,189 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/gorilla/mux"
+
+	"github.com/mtlynch/whatgotdone/backend/datastore/mock"
+	"github.com/mtlynch/whatgotdone/backend/types"
+)
+
+func TestExportPopulatedUserAccount(t *testing.T) {
+	ds := mock.MockDatastore{
+		JournalDrafts: []types.JournalEntry{
+			{
+				Date:         types.EntryDate("2021-11-19"),
+				LastModified: "2021-11-19",
+				Markdown:     "went to the store today",
+			},
+			{
+				Date:         types.EntryDate("2021-11-26"),
+				LastModified: "2021-11-20",
+				Markdown:     "bought a new car",
+			},
+		},
+		JournalEntries: []types.JournalEntry{
+			{
+				Date:         types.EntryDate("2021-11-19"),
+				LastModified: "2021-11-19",
+				Markdown:     "went to the store today",
+			},
+		},
+		UserFollows: map[types.Username][]types.Username{
+			types.Username("dummyUserA"): {types.Username("dummyUserC")},
+		},
+		UserPreferences: map[types.Username]types.Preferences{
+			types.Username("dummyUserA"): {
+				EntryTemplate: "# My weekly template",
+			},
+		},
+		UserProfile: types.UserProfile{
+			AboutMarkdown: "I'm just a dummy user",
+			EmailAddress:  "dummy@example.com",
+		},
+		Usernames: []types.Username{"dummyUserA", "dummyUserB", "dummyUserC"},
+	}
+	router := mux.NewRouter()
+	s := defaultServer{
+		authenticator: mockAuthenticator{
+			tokensToUsers: map[string]types.Username{
+				"mock_token_A": "dummyUserA",
+			},
+		},
+		datastore:      &ds,
+		router:         router,
+		csrfMiddleware: dummyCsrfMiddleware(),
+	}
+	s.routes()
+
+	req, err := http.NewRequest("GET", "/api/export", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Cookie", fmt.Sprintf("%s=mock_token_A", userKitAuthCookieName))
+
+	w := httptest.NewRecorder()
+	s.router.ServeHTTP(w, req)
+
+	if status := w.Code; status != http.StatusOK {
+		t.Fatalf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	var response exportedUserData
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Response is not valid JSON: %v", w.Body.String())
+	}
+
+	exportExpected := exportedUserData{
+		Drafts: []exportedEntry{
+			{
+				Date:         types.EntryDate("2021-11-19"),
+				LastModified: "2021-11-19",
+				Markdown:     "went to the store today",
+			},
+			{
+				Date:         types.EntryDate("2021-11-26"),
+				LastModified: "2021-11-20",
+				Markdown:     "bought a new car",
+			},
+		},
+		Entries: []exportedEntry{
+			{
+				Date:         types.EntryDate("2021-11-19"),
+				LastModified: "2021-11-19",
+				Markdown:     "went to the store today",
+			},
+		},
+		Following: []types.Username{types.Username("dummyUserC")},
+		Preferences: exportedPreferences{
+			EntryTemplate: "# My weekly template",
+		},
+		Profile: profilePublic{
+			AboutMarkdown: "I'm just a dummy user",
+			EmailAddress:  "dummy@example.com",
+		},
+	}
+	if !reflect.DeepEqual(response, exportExpected) {
+		t.Fatalf("Unexpected response: got %+v want %+v", response, exportExpected)
+	}
+}
+
+func TestExportEmptyUserAccount(t *testing.T) {
+	ds := mock.MockDatastore{}
+	router := mux.NewRouter()
+	s := defaultServer{
+		authenticator: mockAuthenticator{
+			tokensToUsers: map[string]types.Username{
+				"mock_token_A": "dummyUserA",
+			},
+		},
+		datastore:      &ds,
+		router:         router,
+		csrfMiddleware: dummyCsrfMiddleware(),
+	}
+	s.routes()
+
+	req, err := http.NewRequest("GET", "/api/export", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Cookie", fmt.Sprintf("%s=mock_token_A", userKitAuthCookieName))
+
+	w := httptest.NewRecorder()
+	s.router.ServeHTTP(w, req)
+
+	if status := w.Code; status != http.StatusOK {
+		t.Fatalf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	var response exportedUserData
+	err = json.Unmarshal(w.Body.Bytes(), &response)
+	if err != nil {
+		t.Fatalf("Response is not valid JSON: %v", w.Body.String())
+	}
+
+	exportExpected := exportedUserData{
+		Entries: []exportedEntry{},
+		Drafts:  []exportedEntry{},
+	}
+	if !reflect.DeepEqual(response, exportExpected) {
+		t.Fatalf("Unexpected response: got %#v want %#v", response, exportExpected)
+	}
+}
+
+func TestExportUnauthenticatedAccount(t *testing.T) {
+	ds := mock.MockDatastore{}
+	router := mux.NewRouter()
+	s := defaultServer{
+		authenticator: mockAuthenticator{
+			tokensToUsers: map[string]types.Username{
+				"mock_token_A": "dummyUserA",
+			},
+		},
+		datastore:      &ds,
+		router:         router,
+		csrfMiddleware: dummyCsrfMiddleware(),
+	}
+	s.routes()
+
+	req, err := http.NewRequest("GET", "/api/export", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Cookie", fmt.Sprintf("%s=mock-invalid-token", userKitAuthCookieName))
+
+	w := httptest.NewRecorder()
+	s.router.ServeHTTP(w, req)
+
+	if status := w.Code; status != http.StatusForbidden {
+		t.Fatalf("handler returned wrong status code: got %v want %v",
+			status, http.StatusForbidden)
+	}
+}
