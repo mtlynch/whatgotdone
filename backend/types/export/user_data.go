dfcefff1d5cea9db99f986d7e8a57ce4d90a58b4
Michael Lynch <git@mtlynch.io>
2021-11-25 09:24:06 -0500

    Fix annotations on exported types

--- backend/types/export/user_data.go
+++ backend/types/export/user_data.go
@@ -3,9 +3,9 @@ package export
 import "github.com/mtlynch/whatgotdone/backend/types"
 
 type UserData struct {
-	Entries     []JournalEntry   `json:"entries"`
-	Drafts      []JournalEntry   `json:"drafts"`
-	Following   []types.Username `json:"following"`
-	Profile     UserProfile      `json:"profile"`
-	Preferences Preferences      `json:"preferences"`
+	Entries     []JournalEntry   `json:"entries" yaml:"entries"`
+	Drafts      []JournalEntry   `json:"drafts" yaml:"drafts"`
+	Following   []types.Username `json:"following" yaml:"following"`
+	Profile     UserProfile      `json:"profile" yaml:"profile"`
+	Preferences Preferences      `json:"preferences" yaml:"preferences"`
 }
--- backend/types/export/user_profile.go
+++ backend/types/export/user_profile.go
@@ -3,8 +3,8 @@ package export
 import "github.com/mtlynch/whatgotdone/backend/types"
 
 type UserProfile struct {
-	AboutMarkdown   types.UserBio         `json:"aboutMarkdown" firestore:"about_markdown"`
-	EmailAddress    types.EmailAddress    `json:"emailAddress" firestore:"email_address"`
-	TwitterHandle   types.TwitterHandle   `json:"twitterHandle" firestore:"twitter_handle"`
+	AboutMarkdown   types.UserBio         `json:"aboutMarkdown" yaml:"about_markdown"`
+	EmailAddress    types.EmailAddress    `json:"emailAddress" yaml:"email_address"`
+	TwitterHandle   types.TwitterHandle   `json:"twitterHandle" yaml:"twitter_handle"`
 	MastodonAddress types.MastodonAddress `json:"mastodonAddress" yaml:"mastodon_address"`
 }
