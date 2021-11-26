f27aeb24c56012885ee418dcba864583aa096188
Michael Lynch <git@mtlynch.io>
2021-11-26 09:17:57 -0500

    Fix pageviews reader to return pageviewsnotfounderror on no rows

--- backend/datastore/sqlite/page_views.go
+++ backend/datastore/sqlite/page_views.go
@@ -3,6 +3,8 @@ package sqlite
 import (
 	"database/sql"
 	"log"
+
+	"github.com/mtlynch/whatgotdone/backend/datastore"
 )
 
 // InsertPageViews stores the count of pageviews for a given What Got Done route.
@@ -34,7 +36,7 @@ func (d db) GetPageViews(path string) (int, error) {
 	var pageViews int
 	err = stmt.QueryRow(path).Scan(&pageViews)
 	if err == sql.ErrNoRows {
-		return 0, nil
+		return 0, datastore.PageViewsNotFoundError{Path: path}
 	} else if err != nil {
 		return 0, err
 	}
