1d83067a0b161c67c4bde5caf9413340a4a7f2d7
Michael Lynch <mtlynch@users.noreply.github.com>
2021-11-27 08:06:03 -0500

    Add support for exporting all data (#696)

---
+++ backend/handlers/admin_dev.go
@@ -0,0 +1,10 @@
+// +build dev integration
+
+package handlers
+
+import "github.com/mtlynch/whatgotdone/backend/types"
+
+// This is a temporary hack while we migrate from AppEngine to fly.io.
+func isAdminUser(username types.Username) bool {
+	return username == "mike"
+}
