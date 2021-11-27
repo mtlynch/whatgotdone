8a0e655d94356398fc58424fb59ccc807670759d
Michael Lynch <git@mtlynch.io>
2021-11-27 10:44:18 -0500

    Revert "Move exported fields of journal entry to export package"
    This reverts commit d743bb721e684b59af8eaad2b724e2de9cf07fcb.

--- backend/types/export/journal_entry.go
+++ backend/types/export/journal_entry.go
@@ -5,5 +5,5 @@ import "github.com/mtlynch/whatgotdone/backend/types"
 type JournalEntry struct {
 	Date         types.EntryDate `json:"date" yaml:"date"`
 	Markdown     string          `json:"markdown" yaml:"markdown"`
-	LastModified string          `json:"lastModified" yaml:"last_modified"`
+	LastModified string          `json:"lastModified"`
 }
--- backend/types/journal_entry.go
+++ backend/types/journal_entry.go
@@ -8,8 +8,8 @@ type (
 	// public or a private draft that has not yet been published.
 	JournalEntry struct {
 		Author       Username
-		Date         EntryDate
-		LastModified string
-		Markdown     string
+		Date         EntryDate `json:"date" yaml:"date"`
+		LastModified string    `json:"lastModified" yaml:"lastModified"`
+		Markdown     string    `json:"markdown" yaml:"markdown"`
 	}
 )
