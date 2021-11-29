e2ebb6e78c64e7f232381c10f120a4546fa96119
Michael Lynch <git@mtlynch.io>
2021-11-28 23:22:38 -0500

    Add CSRF log message in production

--- backend/handlers/csrf_prod.go
+++ backend/handlers/csrf_prod.go
@@ -19,6 +19,7 @@ func getCsrfSeed() string {
 }
 
 func (s defaultServer) enableCsrf(h http.Handler) http.Handler {
+	log.Printf("enabling CSRF protection in production mode")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-CSRF-Token", csrf.Token(r))
 		s.csrfMiddleware(h).ServeHTTP(w, r)
