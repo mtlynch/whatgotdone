b260b2cdc58b221df2b6834949628dff9098ffa3
Michael Lynch <git@mtlynch.io>
2021-11-28 23:18:26 -0500

    Add logging for CSRF loading

--- backend/handlers/csrf_dev.go
+++ backend/handlers/csrf_dev.go
@@ -3,6 +3,7 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/csrf"
@@ -14,6 +15,7 @@ func getCsrfSeed() string {
 }
 
 func (s defaultServer) enableCsrf(h http.Handler) http.Handler {
+	log.Printf("enabling CSRF protection in dev mode")
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// In dev mode, set the X-CSRF-Token on responses, but don't require
 		// requests to send a correct CSRF token back.
