a379397b83ddd1caf7fa6351e6745396088d8420
Michael Lynch <mtlynch@users.noreply.github.com>
2021-12-05 06:15:14 -0500

    Upgrade to go 1.17.4 (#731)
    * Upgrade to go 1.17.4
    
    * Update CircleCI to use go 1.17.4
    
    * Convert build directives to new syntax
    
    * Update go version in go.mod
    
    * Update minimum go version in README

--- backend/handlers/csp_dev.go
+++ backend/handlers/csp_dev.go
@@ -1,4 +1,4 @@
-// +build dev
+//go:build dev
 
 // The dev environment needs extra CSP sources because when Vue generates
 // frontend code in dev mode, it requires directives that would be unsafe in
