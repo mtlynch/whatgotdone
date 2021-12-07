a63e203b81fc001e2f428a0a3fa660b131b88837
Michael Lynch <mtlynch@users.noreply.github.com>
2021-12-06 21:59:35 -0500

    Allow console.log during development but not production (#745)
    * Allow console.log during development but not production
    
    * Install dev dependencies on CircleCI

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
