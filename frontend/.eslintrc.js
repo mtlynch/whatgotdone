4e77082acb26e3dc1f94b5ba21e63120c260e416
Michael Lynch <mtlynch@users.noreply.github.com>
2022-02-06 10:19:57 -0500

    Upgrade npm packages (#766)

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/multi-word-component-names': 'warn',
   },
   parserOptions: {
     parser: 'babel-eslint',
