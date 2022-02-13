104ea5dcb9c5b3a947c665e369347e48c241afc1
Michael Lynch <git@mtlynch.io>
2022-02-13 07:37:29 -0500

    Switch to external social library for parsing Twitter handles

--- backend/handlers/parse/twitter_handle.go
+++ backend/handlers/parse/twitter_handle.go
@@ -2,16 +2,14 @@ package parse
 
 import (
 	"errors"
-	"regexp"
-	"strings"
+
+	"github.com/mtlynch/social-go/v2/social"
 
 	"github.com/mtlynch/whatgotdone/backend/types"
 )
 
 var (
 	ErrInvalidTwitterHandle = errors.New("invalid twitter handle")
-
-	twitterHandlePattern = regexp.MustCompile("^[A-Za-z0-9_]{4,15}$")
 )
 
 // TwitterHandle parses a raw string into a TwitterHandle, validating that it's
@@ -21,10 +19,10 @@ func TwitterHandle(handle string) (types.TwitterHandle, error) {
 		return types.TwitterHandle(""), ErrInvalidTwitterHandle
 	}
 
-	h := strings.TrimPrefix(handle, "@")
-
-	if !twitterHandlePattern.MatchString(h) {
+	h, err := social.ParseTwitterHandle(handle)
+	if err != nil {
 		return types.TwitterHandle(""), ErrInvalidTwitterHandle
 	}
+
 	return types.TwitterHandle(h), nil
 }
