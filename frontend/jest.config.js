1d215b47b84781a6f6cce0c990b47681cfb2fe37
Michael Lynch <git@mtlynch.io>
2022-12-31 13:19:32 -0500

    Fix transform

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
   transform: {
-    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.vue$': '@vue/vue2-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
       'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest',
