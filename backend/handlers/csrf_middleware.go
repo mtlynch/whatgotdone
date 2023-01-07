264ffc5bc6360cfd645c50b6512acec78601f309
Michael Lynch <mtlynch@users.noreply.github.com>
2023-01-07 10:09:52 -0500

    Cycle the CSRF token name (#840)

--- backend/handlers/csrf_middleware.go
+++ backend/handlers/csrf_middleware.go
@@ -10,7 +10,7 @@ func newCsrfMiddleware() httpMiddlewareHandler {
 		[]byte(csrfSeed),
 		// The _v suffix is just to prevent clients from re-using a previous version of the cookie.
 		// When rev'ing the version, be sure to globally replace this name in the entire codebase.
-		csrf.CookieName("csrf_base_v3"),
+		csrf.CookieName("csrf_base_v4"),
 		csrf.Path("/"),
 		csrf.Secure(false))
 }
