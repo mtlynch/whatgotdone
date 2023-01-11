8006873ca6edd24d9e373cc44b70d9c71dce4f09
Michael Lynch <git@mtlynch.io>
2023-01-10 21:31:31 -0500

    Revert change to vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   runtimeCompiler: true,
   devServer: {
-    allowedHosts: 'auto',
+    allowedHosts: 'all',
   },
   // We have to disable removeAttributeQuotes otherwise webpack will
   // prematurely remove quotes from things that actually need them once go
