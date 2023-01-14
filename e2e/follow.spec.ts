e31be3f21006132527cdff113817759374f2ed6f
Michael Lynch <git@mtlynch.io>
2023-01-14 00:38:28 -0500

    Add follow tests

---
+++ e2e/follow.spec.ts
@@ -0,0 +1,54 @@
+import { expect, test } from "@playwright/test";
+import { wipeDB, populateDummyData } from "./helpers/db.js";
+import { loginAsUser } from "./helpers/login.js";
+
+test.beforeEach(async ({ page }) => {
+  await wipeDB(page);
+  await populateDummyData(page);
+});
+
+test("follows a user", async ({ page, baseURL }) => {
+  // Log in as follower user.
+  await loginAsUser(page, "follower_frank");
+  await expect(page).toHaveURL(/\/entry\/edit\/.+/g);
+
+  // Verify the personalized feed is empty.
+  await page.locator("data-test-id=nav-feed-btn").click();
+  await expect(page.locator(".alert")).toContainText(
+    "You're not following anyone yet."
+  );
+  await expect(page.locator(".journal")).toHaveCount(0);
+
+  // Follow leader_lenny
+  let apiFollowingGet = page.waitForRequest(
+    (request) =>
+      request
+        .url()
+        .startsWith(baseURL + "/api/user/follower_frank/following") &&
+      request.method() === "GET"
+  );
+
+  await page.goto("/leader_lenny");
+  await apiFollowingGet;
+  await page.locator("data-test-id=follow-btn").click();
+  await expect(page.locator("data-test-id=unfollow-btn")).toHaveCount(1);
+  await expect(page.locator("data-test-id=follow-btn")).toHaveCount(0);
+
+  // Verify personalized feed is non-empty
+  await page.locator("data-test-id=nav-feed-btn").click();
+  await expect(page).toHaveURL("/feed");
+  await expect(page.locator(".journal")).toHaveCount(2);
+
+  // Unfollow leader_lenny
+  await page.goBack();
+  await expect(page).toHaveURL("/leader_lenny");
+  await page.locator("data-test-id=unfollow-btn").click();
+
+  // Verify the personalized feed is empty again.
+  await page.locator("data-test-id=nav-feed-btn").click();
+  await expect(page).toHaveURL("/feed");
+  await expect(page.locator(".alert")).toContainText(
+    "You're not following anyone yet."
+  );
+  await expect(page.locator(".journal")).toHaveCount(0);
+});
