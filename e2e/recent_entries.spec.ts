33085d8f1e8294f81ce147752b4be232b2bec626
Michael Lynch <git@mtlynch.io>
2023-01-15 07:46:21 -0500

    Add recent entries test

--- e2e/recent_entries.spec.ts
+++ e2e/recent_entries.spec.ts
@@ -1,6 +1,5 @@
 import { expect, test } from "@playwright/test";
 import { wipeDB, populateDummyData } from "./helpers/db.js";
-import { loginAsUser } from "./helpers/login.js";
 
 test.beforeEach(async ({ page }) => {
   await wipeDB(page);
@@ -12,7 +11,10 @@ test("shows recent posts", async ({ page }) => {
   await page.locator("data-test-id=recent-link").click();
   await expect(page).toHaveURL("/recent");
 
-  await expect(page.locator(".journal").first()).toHaveText(
-    "staging_jimmy's update"
+  await expect(page.locator(".journal .journal-header").first()).toContainText(
+    "staging_jimmy's update for the week ending on Friday, Jun 28, 2019"
+  );
+  await expect(page.locator(".journal .journal-body").first()).toContainText(
+    "Today was a productive day. I created a test data manager for What Got Done!"
   );
 });
