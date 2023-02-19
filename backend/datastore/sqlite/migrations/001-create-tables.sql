76b1b8239a020c4ea0ebac6a3ee36ec89c4ef483
Michael Lynch <mtlynch@users.noreply.github.com>
2023-02-18 21:52:43 -0500

    Convert SQL initialization to migrations (#860)

---
+++ backend/datastore/sqlite/migrations/001-create-tables.sql
@@ -0,0 +1,47 @@
+-- Suppress sqlfluff rule about bad column names. I created them before I knew.
+-- L029: Keywords should not be used as identifiers.
+-- noqa: disable=L029
+
+CREATE TABLE IF NOT EXISTS user_preferences (
+    username TEXT PRIMARY KEY,
+    entry_template TEXT
+);
+
+CREATE TABLE IF NOT EXISTS user_profiles (
+    username TEXT PRIMARY KEY,
+    about_markdown TEXT,
+    email TEXT,
+    twitter TEXT,
+    mastodon TEXT
+);
+
+CREATE TABLE IF NOT EXISTS journal_entries(
+    username TEXT,
+    date TEXT,
+    last_modified TEXT,
+    markdown TEXT,
+    is_draft INTEGER,
+    PRIMARY KEY (username, date, is_draft)
+);
+
+CREATE TABLE IF NOT EXISTS follows(
+    follower TEXT,
+    leader TEXT,
+    created TEXT,
+    PRIMARY KEY (leader, follower)
+);
+
+CREATE TABLE IF NOT EXISTS entry_reactions(
+    entry_author TEXT,
+    entry_date TEXT,
+    reacting_user TEXT,
+    reaction TEXT,
+    timestamp TEXT,
+    PRIMARY KEY (entry_author, entry_date, reacting_user)
+);
+
+CREATE TABLE IF NOT EXISTS pageviews(
+    path TEXT PRIMARY KEY,
+    views INTEGER,
+    last_updated TEXT
+);
--- backend/datastore/sqlite/sqlite.go
+++ backend/datastore/sqlite/sqlite.go
@@ -21,62 +21,20 @@ func New(path string) datastore.Datastore {
 		log.Fatalln(err)
 	}
 
-	initStmts := []string{
-		// The Litestream documentation recommends these pragmas.
-		// https://litestream.io/tips/
-		`PRAGMA busy_timeout = 5000`,
-		`PRAGMA synchronous = NORMAL`,
-
-		`CREATE TABLE IF NOT EXISTS user_preferences (
-			username TEXT PRIMARY KEY,
-			entry_template TEXT
-			)`,
-		`CREATE TABLE IF NOT EXISTS user_profiles (
-			username TEXT PRIMARY KEY,
-			about_markdown TEXT,
-			email TEXT,
-			twitter TEXT,
-			mastodon TEXT
-			)`,
-		`CREATE TABLE IF NOT EXISTS journal_entries(
-			username TEXT,
-			date TEXT,
-			last_modified TEXT,
-			markdown TEXT,
-			is_draft INTEGER,
-			PRIMARY KEY (username, date, is_draft)
-			)`,
-		`CREATE TABLE IF NOT EXISTS follows(
-			follower TEXT,
-			leader TEXT,
-			created TEXT,
-			PRIMARY KEY (leader, follower)
-			)`,
-		`CREATE TABLE IF NOT EXISTS entry_reactions(
-			entry_author TEXT,
-			entry_date TEXT,
-			reacting_user TEXT,
-			reaction TEXT,
-			timestamp TEXT,
-			PRIMARY KEY (entry_author, entry_date, reacting_user)
-			)`,
-		`CREATE TABLE IF NOT EXISTS pageviews(
-			path TEXT PRIMARY KEY,
-			views INTEGER,
-			last_updated TEXT
-			)`,
+	// The Litestream documentation recommends these pragmas.
+	// https://litestream.io/tips/
+	if _, err := ctx.Exec(`
+		PRAGMA busy_timeout = 5000;
+		PRAGMA synchronous = NORMAL;
+		`); err != nil {
+		log.Fatalf("failed to set pragmas: %v", err)
 	}
 
-	for _, stmt := range initStmts {
-		_, err = ctx.Exec(stmt)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
+	d := &DB{ctx: ctx}
 
-	return &DB{
-		ctx: ctx,
-	}
+	d.applyMigrations()
+
+	return d
 }
 
 func parseDate(s string) (time.Time, error) {
