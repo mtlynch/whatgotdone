mtlynch/whatgotdone

commit 0c8400db762a2efae0286a15bbddbb37c2cc7124

Make GCP creds option for docker-compose (#863)
docker-compose unintentionally had a dependency on the existence of GCP
service account secrets, which are actually optional. This fixes
docker-compose so that it can run even if GCP service account
credentials are not present on the filesystem.

.circleci/config.yml
@@ -84,10 +84,13 @@ jobs:
     steps:
       - checkout
       - run:
+          name: Create folder for GCP credentials
+          command: mkdir creds/
+      - run:
           name: Retrieve staging service account credentials (JSON) from CircleCI
           command: |
             echo "${INTEGRATION_CLIENT_SECRET}" | \
-              base64 --decode > gcp-service-account-staging.json
+              base64 --decode > creds/gcp-service-account-staging.json
       - attach_workspace:
           at: ./
       - run:
@@ -112,10 +115,13 @@ jobs:
     steps:
       - checkout
       - run:
+          name: Create folder for GCP credentials
+          command: mkdir creds/
+      - run:
           name: Retrieve GCP service account client secret from CircleCI
           command: |
             echo "$GCP_SERVICE_ACCOUNT" | \
-              base64 --decode > gcp-service-account-prod.json
+              base64 --decode > creds/gcp-service-account-prod.json
       - run:
           name: Install flyctl
           command: curl -L https://fly.io/install.sh | sh -s "${FLYCTL_VERSION}"

.gitignore
@@ -7,7 +7,7 @@ node_modules/
 *.sw?
 
 # GCP service account credentials
-*service-account*.json
+creds
 
 # Holds secret environment variables in prod
 env_variables.yaml

Dockerfile
@@ -47,7 +47,6 @@ COPY --from=backend_builder /app/bin/whatgotdone /app/bin/whatgotdone
 COPY --from=litestream_downloader /litestream/litestream /app/litestream
 COPY ./litestream.yml /etc/litestream.yml
 COPY ./docker_entrypoint /app/docker_entrypoint
-COPY ./gcp-service-account-*.json /app/
 
 WORKDIR /app
 

docker-compose.yml
@@ -12,3 +12,5 @@ services:
       - PORT=3001
       - CSRF_SECRET_SEED=dummy-dev-secret-seed
       - USERKIT_SECRET=dummy.dummy
+    volumes:
+      - ./creds:/app/creds

backend/gcp/service_account_dev.go
@@ -2,4 +2,4 @@
 
 package gcp
 
-const ServiceAccountKeyFile = "gcp-service-account-dev.json"
+const ServiceAccountKeyFile = "creds/gcp-service-account-dev.json"

backend/gcp/service_account_prod.go
@@ -2,4 +2,4 @@
 
 package gcp
 
-const ServiceAccountKeyFile = "gcp-service-account-prod.json"
+const ServiceAccountKeyFile = "creds/gcp-service-account-prod.json"

backend/gcp/service_account_staging.go
@@ -2,4 +2,4 @@
 
 package gcp
 
-const ServiceAccountKeyFile = "gcp-service-account-staging.json"
+const ServiceAccountKeyFile = "creds/gcp-service-account-staging.json"

commit 793004e191c4cd66891f7a841547704045801561

Build credentials folder into Docker container (#866)
Fixes a regression caused by 0c8400db762a2efae0286a15bbddbb37c2cc7124
that prevented production builds from keeping GCP creds and using GCS
buckets.

.circleci/config.yml
@@ -84,9 +84,6 @@ jobs:
     steps:
       - checkout
       - run:
-          name: Create folder for GCP credentials
-          command: mkdir creds/
-      - run:
           name: Retrieve staging service account credentials (JSON) from CircleCI
           command: |
             echo "${INTEGRATION_CLIENT_SECRET}" | \
@@ -114,9 +111,6 @@ jobs:
       FLYCTL_VERSION: "latest"
     steps:
       - checkout
-      - run:
-          name: Create folder for GCP credentials
-          command: mkdir creds/
       - run:
           name: Retrieve GCP service account client secret from CircleCI
           command: |

.dockerignore
@@ -2,3 +2,4 @@ bin
 frontend/dist
 frontend/node_modules
 data/
+creds/README.md

.gitignore
@@ -7,7 +7,8 @@ node_modules/
 *.sw?
 
 # GCP service account credentials
-creds
+creds/*
+!creds/README.md
 
 # Holds secret environment variables in prod
 env_variables.yaml

Dockerfile
@@ -45,6 +45,7 @@ RUN apk add --no-cache bash
 COPY --from=frontend_builder /app/frontend/dist /app/frontend/dist
 COPY --from=backend_builder /app/bin/whatgotdone /app/bin/whatgotdone
 COPY --from=litestream_downloader /litestream/litestream /app/litestream
+COPY ./creds /app/creds
 COPY ./litestream.yml /etc/litestream.yml
 COPY ./docker_entrypoint /app/docker_entrypoint
 

docker-compose.yml
@@ -12,5 +12,3 @@ services:
       - PORT=3001
       - CSRF_SECRET_SEED=dummy-dev-secret-seed
       - USERKIT_SECRET=dummy.dummy
-    volumes:
-      - ./creds:/app/creds