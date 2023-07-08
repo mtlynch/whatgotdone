f218c5ca45964028bbafa433441da1b64330ca4a
Michael Lynch <git@mtlynch.io>
2023-07-07 21:38:56 -0400

    Add Nix flake for WhatGotDone

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Dev environment for WhatGotDone";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [
+        (self: super: rec {
+          nodejs = super.nodejs-18_x;
+        })
+      ];
+      pkgs = import nixpkgs { inherit overlays system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [ node2nix nodejs-18_x go_1_19 ];
+
+        shellHook = ''
+          echo "node `${pkgs.nodejs}/bin/node --version`"
+        '';
+      };
+    });
+}
