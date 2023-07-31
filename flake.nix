bbfe973f2b2b9ce426d7feb4050ff01754f92a09
Michael Lynch <git@mtlynch.io>
2023-07-31 08:00:59 -0400

    Get go tests working

--- flake.nix
+++ flake.nix
@@ -32,8 +32,10 @@
     {
       devShells.default = pkgs_for_go.mkShell.override { stdenv = pkgs_for_go.pkgsStatic.stdenv; } {
         packages = with pkgs; [
+          glibc.static
           gopls
           gotools
+          musl
           pkgs_for_go.go_1_19
           pkgs_for_nodejs.nodejs-18_x
           pkgs_for_sqlfluff.sqlfluff
