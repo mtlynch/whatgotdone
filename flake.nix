be53555c6b2308b27d564927f7b09998125b508d
Michael Lynch <git@mtlynch.io>
2023-07-30 21:44:20 -0400

    Pin versions - Doesn't work - sqlfluff getting 1.4.2

--- flake.nix
+++ flake.nix
@@ -12,24 +12,37 @@
       pkgs = import nixpkgs { inherit system; };
 
       pkgs_for_sqlfluff = import (builtins.fetchTarball {
+        # 1.2.1 release
+        url = "https://github.com/NixOS/nixpkgs/archive/7cf5ccf1cdb2ba5f08f0ac29fc3d04b0b59a07e4.tar.gz";
+        sha256 = "0p7m72ipxyya5nn2p8q6h8njk0qk0jhmf6sbfdiv4sh05mbndj4q";
+      }) {inherit system; };
+
+      pkgs_for_go = import (builtins.fetchTarball {
+        # 1.19.6 release
         url = "https://github.com/NixOS/nixpkgs/archive/6adf48f53d819a7b6e15672817fa1e78e5f4e84f.tar.gz";
         sha256 = "0p7m72ipxyya5nn2p8q6h8njk0qk0jhmf6sbfdiv4sh05mbndj4q";
-      }) {};
+      }) {inherit system; };
+
+      pkgs_for_nodejs = import (builtins.fetchTarball {
+        # 18.14.1 release
+        url = "https://github.com/NixOS/nixpkgs/archive/6adf48f53d819a7b6e15672817fa1e78e5f4e84f.tar.gz";
+        sha256 = "0p7m72ipxyya5nn2p8q6h8njk0qk0jhmf6sbfdiv4sh05mbndj4q";
+      }) {inherit system; };
     in
     {
-      devShells.default = pkgs.mkShell.override { stdenv = pkgs.pkgsStatic.stdenv; } {
+      devShells.default = pkgs_for_go.mkShell.override { stdenv = pkgs_for_go.pkgsStatic.stdenv; } {
         packages = with pkgs; [
-          go_1_19
           gopls
           gotools
-          node2nix
-          nodejs
+          pkgs_for_go.go_1_19
+          pkgs_for_nodejs.nodejs-18_x
           pkgs_for_sqlfluff.sqlfluff
         ];
 
         shellHook = ''
           echo "node" "$(node --version)"
           go version
+          sqlfluff --version
         '';
       };
     });
