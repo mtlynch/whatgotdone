63ea6b573d070554fcaf7e0b0325dec572c6cb48
Michael Lynch <git@mtlynch.io>
2023-09-29 21:59:37 -0400

    Remove unused input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Dev environment for WhatGotDone";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
 
     # 1.19.6 release
@@ -15,20 +14,19 @@
     sqlfluff_dep.url = "github:NixOS/nixpkgs/7cf5ccf1cdb2ba5f08f0ac29fc3d04b0b59a07e4";
   };
 
-  outputs = { self, nixpkgs, flake-utils, go_dep, nodejs_dep, sqlfluff_dep }@inputs :
+  outputs = { self, flake-utils, go_dep, nodejs_dep, sqlfluff_dep }@inputs :
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
       go_dep = inputs.go_dep.legacyPackages.${system};
       sqlfluff_dep = inputs.sqlfluff_dep.legacyPackages.${system};
       nodejs_dep = inputs.nodejs_dep.legacyPackages.${system};
     in
     {
       devShells.default = go_dep.mkShell.override { stdenv = go_dep.pkgsStatic.stdenv; } {
-        packages = with pkgs; [
-          gopls
-          gotools
+        packages = [
           go_dep.go_1_19
+          go_dep.gopls
+          go_dep.gotools
           nodejs_dep.nodejs-18_x
           sqlfluff_dep.sqlfluff
         ];
