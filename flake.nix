8b6410d792d5f9b53ca03d161ff13a154865467d
Michael Lynch <git@mtlynch.io>
2023-09-29 21:19:03 -0400

    Adjust flake inputs

--- flake.nix
+++ flake.nix
@@ -4,39 +4,33 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
+
+    # 1.19.6 release
+    go_dep.url = "github:NixOS/nixpkgs/6adf48f53d819a7b6e15672817fa1e78e5f4e84f";
+
+    # 18.14.1 release
+    nodejs_dep.url = "github:NixOS/nixpkgs/6adf48f53d819a7b6e15672817fa1e78e5f4e84f";
+
+    # 1.2.1 release
+    sqlfluff_dep.url = "github:NixOS/nixpkgs/7cf5ccf1cdb2ba5f08f0ac29fc3d04b0b59a07e4";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, go_dep, nodejs_dep, sqlfluff_dep }@inputs :
     flake-utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import nixpkgs { inherit system; };
-
-      pkgs_for_sqlfluff = import (builtins.fetchTarball {
-        # 1.2.1 release
-        url = "https://github.com/NixOS/nixpkgs/archive/7cf5ccf1cdb2ba5f08f0ac29fc3d04b0b59a07e4.tar.gz";
-        sha256 = "0wfaqjpi7bip86r2piqigqna1fx3m1d9riak4l3rm54lyjxprlpi";
-      }) {inherit system; };
-
-      pkgs_for_go = import (builtins.fetchTarball {
-        # 1.19.6 release
-        url = "https://github.com/NixOS/nixpkgs/archive/6adf48f53d819a7b6e15672817fa1e78e5f4e84f.tar.gz";
-        sha256 = "0p7m72ipxyya5nn2p8q6h8njk0qk0jhmf6sbfdiv4sh05mbndj4q";
-      }) {inherit system; };
-
-      pkgs_for_nodejs = import (builtins.fetchTarball {
-        # 18.14.1 release
-        url = "https://github.com/NixOS/nixpkgs/archive/6adf48f53d819a7b6e15672817fa1e78e5f4e84f.tar.gz";
-        sha256 = "0p7m72ipxyya5nn2p8q6h8njk0qk0jhmf6sbfdiv4sh05mbndj4q";
-      }) {inherit system; };
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      go_dep = inputs.go_dep.legacyPackages.${system};
+      sqlfluff_dep = inputs.sqlfluff_dep.legacyPackages.${system};
+      nodejs_dep = inputs.nodejs_dep.legacyPackages.${system};
     in
     {
-      devShells.default = pkgs_for_go.mkShell.override { stdenv = pkgs_for_go.pkgsStatic.stdenv; } {
+      devShells.default = go_dep.mkShell.override { stdenv = go_dep.pkgsStatic.stdenv; } {
         packages = with pkgs; [
           gopls
           gotools
-          pkgs_for_go.go_1_19
-          pkgs_for_nodejs.nodejs-18_x
-          pkgs_for_sqlfluff.sqlfluff
+          go_dep.go_1_19
+          nodejs_dep.nodejs-18_x
+          sqlfluff_dep.sqlfluff
         ];
 
         shellHook = ''
