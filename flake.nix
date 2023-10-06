51ea9cf59c5a228b9bfc49b965c6864d3e5111dd
Michael Lynch <git@mtlynch.io>
2023-10-06 12:54:59 -0400

    Try a single nixpkg

--- flake.nix
+++ flake.nix
@@ -7,19 +7,12 @@
     # 1.19.6 release
     go_dep.url = "github:NixOS/nixpkgs/6adf48f53d819a7b6e15672817fa1e78e5f4e84f";
 
-    # 18.14.1 release
-    nodejs_dep.url = "github:NixOS/nixpkgs/6adf48f53d819a7b6e15672817fa1e78e5f4e84f";
-
-    # 1.2.1 release
-    sqlfluff_dep.url = "github:NixOS/nixpkgs/7cf5ccf1cdb2ba5f08f0ac29fc3d04b0b59a07e4";
   };
 
-  outputs = { self, flake-utils, go_dep, nodejs_dep, sqlfluff_dep }@inputs :
+  outputs = { self, flake-utils, go_dep,  }@inputs :
     flake-utils.lib.eachDefaultSystem (system:
     let
       go_dep = inputs.go_dep.legacyPackages.${system};
-      sqlfluff_dep = inputs.sqlfluff_dep.legacyPackages.${system};
-      nodejs_dep = inputs.nodejs_dep.legacyPackages.${system};
     in
     {
       devShells.default = go_dep.mkShell.override { stdenv = go_dep.pkgsStatic.stdenv; } {
@@ -27,8 +20,8 @@
           go_dep.go_1_19
           go_dep.gopls
           go_dep.gotools
-          nodejs_dep.nodejs-18_x
-          sqlfluff_dep.sqlfluff
+          go_dep.nodejs-18_x
+          go_dep.sqlfluff
         ];
 
         shellHook = ''
