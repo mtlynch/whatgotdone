38985768bac725d2ef191c279bc1af5ff9952e56
Michael Lynch <git@mtlynch.io>
2024-11-02 21:48:04 -0400

    Fix race condition on preferences page

--- frontend/src/views/UserPreferencesPage.vue
+++ frontend/src/views/UserPreferencesPage.vue
@@ -10,7 +10,8 @@
         <textarea-autosize
           class="form-control"
           id="entry-template-input"
-          v-model="preferences.entryTemplate"
+          v-model="entryTemplate"
+          :disabled="entryTemplate === undefined"
           name="entry-template"
           @input="onEntryTemplateChanged"
           :min-height="250"
@@ -23,7 +24,7 @@
         <button
           type="submit"
           class="btn btn-primary"
-          :disabled="!preferencesChanged"
+          :disabled="!templateChanged"
         >
           Save
         </button>
@@ -54,12 +55,10 @@ export default {
   name: 'UserPreferencesPage',
   data() {
     return {
-      preferences: {
-        entryTemplate: '',
-      },
-      preferencesFromServer: null,
+      entryTemplate: undefined,
+      entryTemplateFromServer: undefined,
       savingPreferences: false,
-      preferencesChanged: false,
+      templateChanged: false,
       preferencesSaved: false,
     };
   },
@@ -72,19 +71,20 @@ export default {
     loadPreferences() {
       getPreferences()
         .then((preferences) => {
-          this.preferencesFromServer = Object.assign({}, preferences);
-          this.preferences = Object.assign({}, preferences);
+          this.entryTemplateFromServer = preferences.entryTemplate;
+          this.entryTemplate = preferences.entryTemplate;
         })
         // Ignore errors on pulling down preferences.
-        .catch(() => {});
+        .catch(() => {
+          this.entryTemplate = '';
+        });
     },
     onEntryTemplateChanged() {
       if (
-        this.preferencesFromServer &&
-        this.preferencesFromServer.entryTemplate !=
-          this.preferences.entryTemplate
+        this.entryTemplateFromServer &&
+        this.entryTemplateFromServer !== this.entryTemplate
       ) {
-        this.preferencesChanged = true;
+        this.templateChanged = true;
       }
     },
     handleSubmit() {
@@ -92,7 +92,7 @@ export default {
       savePreferences(this.preferences)
         .then(() => {
           this.preferencesSaved = true;
-          this.preferencesChanged = false;
+          this.templateChanged = false;
         })
         .finally(() => {
           this.savingPreferences = false;
