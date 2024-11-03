ee627910a8cbc0485b7ede3c0c156856151c0efe
Michael Lynch <git@mtlynch.io>
2024-11-02 21:08:29 -0400

    Rely less on test ID values in e2e tests

--- e2e/entry_template.spec.ts
+++ e2e/entry_template.spec.ts
@@ -29,22 +29,23 @@ test("uses the entry template for new drafts", async ({ page }) => {
 
   await expect(page.getByRole("textbox")).toContainText("");
 
-  // Set an entry template on the preferences page.
-  await page.getByTestId("account-dropdown").click();
-  await page.getByTestId("preferences-link").click();
+  // Navigate to preferences page using the navigation menu.
+  await page.getByRole("button", { name: /account/i }).click();
+  await page.getByRole("link", { name: /preferences/i }).click();
   await expect(page).toHaveURL("/preferences");
 
   await expect(page.getByRole("textbox")).toContainText("");
 
+  // Set template text.
   await page
     .getByRole("textbox")
     .fill("# Example project\n\n* Item A\n* Item B");
   await page.getByRole("button", { name: /save/i }).click();
 
-  await expect(page.locator(".alert-success")).toBeVisible();
+  // Verify success message.
+  await expect(page.getByRole("alert")).toBeVisible();
 
   // Verify new entries start with the template.
-
   apiDraftGet = page.waitForRequest(
     (request) =>
       request.url().includes("/api/draft") && request.method() === "GET"
@@ -52,7 +53,7 @@ test("uses the entry template for new drafts", async ({ page }) => {
   await page.goto("/entry/edit/2020-03-06");
   await apiDraftGet;
 
-  await expect(page.locator(".markdown-editor .editor-textarea")).toHaveValue(
+  await expect(page.getByRole("textbox")).toHaveValue(
     "# Example project\n\n* Item A\n* Item B"
   );
 });
